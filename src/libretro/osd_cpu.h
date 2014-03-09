38da97da1db22dc2ea2f505a19a1cb43975001f4
twinaphex <libretro@gmail.com>
2014-03-09 06:38:31 +0100

    (Win32) Implementation for S_ISDIR

--- src/libretro/osd_cpu.h
+++ src/libretro/osd_cpu.h
@@ -39,6 +39,14 @@
 #endif
 #define strcasecmp stricmp
 #define snprintf _snprintf
+
+#ifndef S_ISDIR
+#define S_ISDIR(mode)  (((mode) & S_IFMT) == S_IFDIR)
+#endif
+
+#ifndef S_ISREG
+#define S_ISREG(mode)  (((mode) & S_IFMT) == S_IFREG)
+#endif
 #endif
 
 
