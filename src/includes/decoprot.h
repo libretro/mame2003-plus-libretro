b755dea17c20e590da259f1f3aae3eda073be73c
gamez-fan <sy@sy1873.plus.com>
2017-12-29 21:32:48 +0000

    Pocket Gal DX protection

--- src/includes/decoprot.h
+++ src/includes/decoprot.h
@@ -6,6 +6,7 @@ READ16_HANDLER( dietgo_104_prot_r );
 READ16_HANDLER( deco16_104_rohga_prot_r );
 READ16_HANDLER( deco16_146_funkyjet_prot_r );
 READ16_HANDLER( deco16_146_nitroball_prot_r );
+READ16_HANDLER( deco16_104_pktgaldx_prot_r );
 READ32_HANDLER( deco32_fghthist_prot_r );
 
 WRITE16_HANDLER( deco16_60_prot_w );
@@ -16,6 +17,7 @@ WRITE16_HANDLER( dietgo_104_prot_w );
 WRITE16_HANDLER( deco16_104_rohga_prot_w );
 WRITE16_HANDLER( deco16_146_funkyjet_prot_w );
 WRITE16_HANDLER( deco16_146_nitroball_prot_w );
+WRITE16_HANDLER( deco16_104_pktgaldx_prot_w );
 WRITE32_HANDLER( deco32_fghthist_prot_w );
 
 extern data16_t *deco16_prot_ram;
