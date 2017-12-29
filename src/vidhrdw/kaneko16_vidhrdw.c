6218f3e6d5fce881f09d2ec6ed2c4a257d754768
gamez-fan <sy@sy1873.plus.com>
2017-12-29 20:38:05 +0000

    Big Kaneko16 update

--- src/vidhrdw/kaneko16_vidhrdw.c
+++ src/vidhrdw/kaneko16_vidhrdw.c
@@ -49,7 +49,7 @@ data16_t *kaneko16_vram_0,    *kaneko16_vram_1,    *kaneko16_layers_0_regs;
 data16_t *kaneko16_vscroll_0, *kaneko16_vscroll_1;
 data16_t *kaneko16_vram_2,    *kaneko16_vram_3,    *kaneko16_layers_1_regs;
 data16_t *kaneko16_vscroll_2, *kaneko16_vscroll_3;
-
+int kaneko16_sprite_fliptype = 0;
 
 int kaneko16_sprite_type;
 data16_t kaneko16_sprite_xoffs, kaneko16_sprite_flipx;
@@ -236,6 +236,55 @@ VIDEO_START( sandscrp_1xVIEW2 )
 	return 0;
 }
 
+VIDEO_START( wingforce_1xVIEW2 )
+{
+	if (	video_start_kaneko16_sprites()	)
+		return 1;
+
+	kaneko16_tmap_0 = tilemap_create(	get_tile_info_0, tilemap_scan_rows,
+										TILEMAP_TRANSPARENT, 16,16, 0x20,0x20	);
+	kaneko16_tmap_1 = tilemap_create(	get_tile_info_1, tilemap_scan_rows,
+										TILEMAP_TRANSPARENT, 16,16, 0x20,0x20	);
+
+	kaneko16_tmap_2 = 0;
+
+	kaneko16_tmap_3 = 0;
+
+	if (	!kaneko16_tmap_0 || !kaneko16_tmap_1	)
+		return 1;
+
+	{
+		int dx, xdim = Machine->drv->screen_width;
+		int dy, ydim = Machine->drv->screen_height;
+
+		switch (xdim)
+		{
+			case 320:	dx = 0x33;	break;
+			case 256:	dx = 0x5b;	break;
+			default:	dx = 0;
+		}
+		switch (Machine->visible_area.max_y - Machine->visible_area.min_y + 1)
+		{
+			case 240- 8:	dy = +0x08;	break;	/* blazeon */
+			case 240-16:	dy = +0x08+1;	break;	/* wing force */
+			default:		dy = 0;
+		}
+
+		tilemap_set_scrolldx( kaneko16_tmap_0, -dx,		xdim + dx -1        );
+		tilemap_set_scrolldx( kaneko16_tmap_1, -(dx+2),	xdim + (dx + 2) - 1 );
+
+		tilemap_set_scrolldy( kaneko16_tmap_0, -dy,		ydim + dy -1 );
+		tilemap_set_scrolldy( kaneko16_tmap_1, -dy,		ydim + dy -1 );
+
+		tilemap_set_transparent_pen(kaneko16_tmap_0, 0);
+		tilemap_set_transparent_pen(kaneko16_tmap_1, 0);
+
+		tilemap_set_scroll_rows(kaneko16_tmap_0, 0x200);	// Line Scroll
+		tilemap_set_scroll_rows(kaneko16_tmap_1, 0x200);
+
+		return 0;
+	}
+}
 
 /* Berlwall has an additional hi-color background */
 
@@ -433,7 +482,6 @@ else
 							( (attr & 0x8000) ? USE_LATCHED_CODE  : 0 ) ;
 }
 
-
 int kaneko16_parse_sprite_type3(int i, struct tempsprite *s)
 {
 	int attr;
@@ -520,8 +568,12 @@ void kaneko16_draw_sprites(struct mame_bitmap *bitmap, const struct rectangle *c
 			s->priority		=	priority;
 			s->xoffs		=	xoffs;
 			s->yoffs		=	yoffs;
-			s->flipx		=	flipx;
-			s->flipy		=	flipy;
+
+			if (kaneko16_sprite_fliptype==0)
+			{
+				s->flipx		=	flipx;
+				s->flipy		=	flipy;
+			}
 		}
 		else
 		{
@@ -529,6 +581,17 @@ void kaneko16_draw_sprites(struct mame_bitmap *bitmap, const struct rectangle *c
 			priority	=	s->priority;
 			xoffs		=	s->xoffs;
 			yoffs		=	s->yoffs;
+
+			if (kaneko16_sprite_fliptype==0)
+			{
+				flipx = s->flipx;
+				flipy = s->flipy;
+			}
+		}
+
+		// brap boys explicitly doesn't want the flip to be latched, maybe there is a different bit to enable that behavior?
+		if (kaneko16_sprite_fliptype==1)
+		{
 			flipx		=	s->flipx;
 			flipy		=	s->flipy;
 		}
