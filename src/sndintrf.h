a7dcc0b897f424c1a5c6763a498bb7b2d8259c0d
arcadez <35177309+arcadez@users.noreply.github.com>
2018-03-16 14:30:04 +0000

    ICS2115 Soundcore

--- src/sndintrf.h
+++ src/sndintrf.h
@@ -179,6 +179,9 @@ struct MachineSound
 #if (HAS_YMF271)
 #include "sound/ymf271.h"
 #endif
+#if (HAS_ICS2115)
+#include "sound/ics2115.h"
+#endif
 
 #ifdef MESS
 #if (HAS_BEEP)
@@ -384,6 +387,9 @@ enum
 #endif
 #if (HAS_YMF271)
 	SOUND_YMF271,
+#endif
+#if (HAS_ICS2115)
+	SOUND_ICS2115,
 #endif
 
 #ifdef MESS
