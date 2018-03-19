3f1f7392a7a4f4d0a671f72011358aaf7d15de91
arcadez <35177309+arcadez@users.noreply.github.com>
2018-03-19 00:01:13 +0000

    Crystal Castles Joystick Version

--- src/driver.c
+++ src/driver.c
@@ -3823,6 +3823,7 @@ BOMULEUL CHAJARA SEGA ST-V  1997/04/11
 	DRIVER( ccastles )	/* 136022			(c) 1983 */
 	DRIVER( ccastle3 )	/* 136022			(c) 1983 */
 	DRIVER( ccastle2 )	/* 136022			(c) 1983 */
+	DRIVER( ccastlej )	/* 136022			(c) 1983 */
 	DRIVER( cloak )		/* 136023			(c) 1983 */
 	DRIVER( cloaksp )	/* 136023			(c) 1983 */
 	DRIVER( cloakfr )	/* 136023			(c) 1983 */
