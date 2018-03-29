8b32ca2aead8669a1933290acf5d3f4f3fa32a1b
markwkidd <mark@stardart.net>
2018-03-29 00:51:05 -0400

    Update info.h

--- src/info.h
+++ src/info.h
@@ -5,6 +5,6 @@
 void print_mame_info(FILE* out, const struct GameDriver* games[]);
 
 /* Print the MAME database in XML format */
-void print_mame_xml(FILE* out, const struct GameDriver* games[]);
+void print_mame_xml(const struct GameDriver* games[]);
 
 #endif
