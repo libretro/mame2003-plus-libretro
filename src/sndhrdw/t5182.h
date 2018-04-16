9e79e2f7e1b967f7767d4e18682cb38813908306
arcadez <35177309+arcadez@users.noreply.github.com>
2018-04-16 19:43:36 +0100

    some changes to soundcore hooks

--- src/sndhrdw/t5182.h
+++ src/sndhrdw/t5182.h
@@ -2,11 +2,11 @@
 
 MACHINE_DRIVER_EXTERN( t5182_audio );
 
-WRITE_HANDLER( t5182_sound_irq_w );
-READ_HANDLER(t5182_sharedram_semaphore_snd_r);
-WRITE_HANDLER(t5182_sharedram_semaphore_main_acquire_w);
-WRITE_HANDLER(t5182_sharedram_semaphore_main_release_w);
+extern WRITE_HANDLER(t5182_sound_irq_w );
+extern READ_HANDLER(t5182_sharedram_semaphore_snd_r);
+extern READ_HANDLER(t5182shared_r);
+extern WRITE_HANDLER(t5182_sharedram_semaphore_main_acquire_w);
+extern WRITE_HANDLER(t5182_sharedram_semaphore_main_release_w);
+extern WRITE_HANDLER(t5182shared_w);
 
 extern UINT8 *t5182_sharedram;
-
-extern const struct YM2151interface t5182_ym2151_interface;
