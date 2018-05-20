5c965dc3a2ce11b27d450b1444789d58795c4add
markwkidd <mark@stardart.net>
2018-05-20 02:24:03 -0500

    provide on-screen error and more robust handline for new option

--- src/mame2003/mame2003.c
+++ src/mame2003/mame2003.c
@@ -323,14 +323,19 @@ static void update_variables(bool first_time)
     if(path_is_valid(cfg_file_path))
     {
       if(!remove(cfg_file_path) == 0)
+      {
         log_cb(RETRO_LOG_ERROR, LOGPRE "%s.cfg exists but cannot be deleted!\n", options.romset_filename_noext);
-    }
-    else
-    {
-      log_cb(RETRO_LOG_INFO, LOGPRE "%s.cfg deleted. Reloading input maps.\n", options.romset_filename_noext);
-      usrintf_showmessage_secs(4, "%s.cfg deleted. Reloading input maps.", options.romset_filename_noext);
-      load_input_port_settings();
-      old_dual_joystick_state = options.dual_joysticks;
+        usrintf_showmessage_secs(4, "ERROR: %s.cfg exists but cannot be deleted!", options.romset_filename_noext);
+        load_input_port_settings();
+        old_dual_joystick_state = options.dual_joysticks;        
+      }
+      else
+      {
+        log_cb(RETRO_LOG_INFO, LOGPRE "%s.cfg deleted. Reloading input maps.\n", options.romset_filename_noext);
+        usrintf_showmessage_secs(4, "%s.cfg deleted. Reloading input maps.", options.romset_filename_noext);
+        load_input_port_settings();
+        old_dual_joystick_state = options.dual_joysticks;
+      }
     }
   }
   
