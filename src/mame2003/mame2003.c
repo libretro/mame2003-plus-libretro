793022c4a6e0d51ca2408d643712e03682bf75ab
markwkidd <mark@stardart.net>
2018-05-20 14:05:45 -0500

    no restart needed for dual joy

--- src/mame2003/mame2003.c
+++ src/mame2003/mame2003.c
@@ -109,7 +109,7 @@ void retro_set_environment(retro_environment_t cb)
     { APPNAME"_enable_backdrop", "EXPERIMENTAL: Use Backdrop artwork (Restart); disabled|enabled" },
     { APPNAME"_bios_region", "Specify alternate BIOS region (Restart); default|asia|asia-aes|debug|europe|europe_a|japan|japan_a|japan_b|taiwan|us|us_a|uni-bios.10|uni-bios.11|uni-bios.13|uni-bios.20" },
     { APPNAME"_dialsharexy", "Share 2 player dial controls across one X/Y device; disabled|enabled" },
-    { APPNAME"_dual_joysticks", "Dual Joystick Mode (Players 1 & 2) (Restart); disabled|enabled" },
+    { APPNAME"_dual_joysticks", "Dual Joystick Mode (Players 1 & 2); disabled|enabled" },
     { APPNAME"_rstick_to_btns", "Right Stick to Buttons; enabled|disabled" },
     { APPNAME"_tate_mode", "TATE Mode; disabled|enabled" },
     { APPNAME"_vector_resolution_multiplier", "EXPERIMENTAL: Vector resolution multiplier (Restart); 1|2|3|4|5|6" },
