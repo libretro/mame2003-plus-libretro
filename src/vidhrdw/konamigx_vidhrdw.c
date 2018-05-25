1c839a9bc995f65a5362b470e05c3b47ae656b54
arcadez <35177309+arcadez@users.noreply.github.com>
2018-05-25 21:21:23 +0100

    Fix Build Errors

--- src/vidhrdw/konamigx_vidhrdw.c
+++ src/vidhrdw/konamigx_vidhrdw.c
@@ -484,8 +484,8 @@ VIDEO_UPDATE(konamigx)
 
 	if( gx_invertlayersBC )
 	{
-		/*draw_crosshair( bitmap, readinputport( 9)*287/0xff+24, readinputport(10)*223/0xff+16, cliprect, 0 );
-		draw_crosshair( bitmap, readinputport(11)*287/0xff+24, readinputport(12)*223/0xff+16, cliprect, 1 );*/
+		draw_crosshair( bitmap, readinputport( 9)*287/0xff+24, readinputport(10)*223/0xff+16, cliprect );
+		draw_crosshair( bitmap, readinputport(11)*287/0xff+24, readinputport(12)*223/0xff+16, cliprect );
 	}
 }
 
