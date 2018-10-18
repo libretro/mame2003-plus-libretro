bd3c6541d291d5121927ef4f3eeddd1da386c9ad
数码视讯Q5 <SumavisionQ5@users.noreply.github.com>
2018-10-18 15:48:13 +0800

    增加《仙境物语》汉化版
    盛大汉化？未测试！

--- src/drivers/flstory.c
+++ src/drivers/flstory.c
@@ -754,8 +754,36 @@ ROM_START( onna34ra )
 	ROM_LOAD( "a52-11.32v",   0x1c000, 0x4000, CRC(d1dda6b3) SHA1(fadf1404e8a03ec7e3fafb6281d33bc73bb5c473) )
 ROM_END
 
+ /*******************
+ Hack of The FairyLand Story   
+**********************/
+
+ROM_START( flstoryjs01 )
+	ROM_REGION( 0x10000, REGION_CPU1, 0 )	/* 64k for the first CPU */
+	ROM_LOAD( "cpu-a45_ps01.15",   0x0000, 0x4000, CRC(1be138fa) SHA1(eaf3b2810d7f4a95930024ec440bf2ba59230e2d) )
+	ROM_LOAD( "cpu-a45_ps01.16",   0x4000, 0x4000, CRC(74b3f227) SHA1(f8ba5c5db92cda691febecdf62b148157adf744a) )
+	ROM_LOAD( "cpu-a45_ps01.17",   0x8000, 0x4000, CRC(1309b079) SHA1(0b82af55273aba6433a789b1d6cfbda650ab85c9) )
+
+	ROM_REGION( 0x10000, REGION_CPU2, 0 )	/* 64k for the second CPU */
+	ROM_LOAD( "a45_12.8",     0x0000, 0x2000, CRC(d6f593fb) SHA1(8551ef22c2cdd9df8d7949a178883f56ea56a4a2) )
+	ROM_LOAD( "a45_13.9",     0x2000, 0x2000, CRC(451f92f9) SHA1(f4196e6d3420983b74001303936d086a48b10827) )
+
+	ROM_REGION( 0x0800, REGION_CPU3, 0 )	/* 2k for the microcontroller */
+	ROM_LOAD( "a45-20.mcu",   0x0000, 0x0800, CRC(7d2cdd9b) SHA1(b9a7b4c7d9d58b4b7cab1304beaa9d17f9559419) )
+
+	ROM_REGION( 0x20000, REGION_GFX1, ROMREGION_DISPOSE | ROMREGION_INVERT )
+	ROM_LOAD( "vid-a45.18",   0x00000, 0x4000, CRC(6f08f69e) SHA1(8f1b7e63a38f855cf26d57aed678da7cf1378fdf) )
+	ROM_LOAD( "vid-a45_ps01.06",   0x04000, 0x4000, CRC(04be939f) SHA1(eee61a1de548a9b9d368b689426f564d7d6680f4) )
+	ROM_LOAD( "vid-a45.08",   0x08000, 0x4000, CRC(d0b028ca) SHA1(c8bd9136ad3180002961ecfe600fc91a3c891539) )
+	ROM_LOAD( "vid-a45_ps01.20",   0x0c000, 0x4000, CRC(51d53e2f) SHA1(30c7665a443af6071a56f64409fea58ace77f547) )
+	ROM_LOAD( "vid-a45.19",   0x10000, 0x4000, CRC(2b572dc9) SHA1(9e14428663819e18829c625b4ae91a8a5530eb33) )
+	ROM_LOAD( "vid-a45_ps01.07",   0x14000, 0x4000, CRC(20f02f0e) SHA1(ff5c3f171ff9b72fc9d03d776f0e8f16205f4a79) )
+	ROM_LOAD( "vid-a45.09",   0x18000, 0x4000, CRC(8336be58) SHA1(b92d37856870c4128a860d8ae02fa647743b99e3) )
+	ROM_LOAD( "vid-a45_ps01.21",   0x1c000, 0x4000, CRC(b6e3caca) SHA1(7f045f0d51216710a68093285cafef7b4aeb4b8e) )
+ROM_END
 
 GAMEX( 1985, flstory,  0,        flstory,  flstory,  0, ROT180, "Taito", "The FairyLand Story", GAME_IMPERFECT_SOUND )
+GAMEX( 200?, flstoryjs01,  flstory,  flstory,flstory,  0, ROT180, "Unknown", "The FairyLand Story (Translation Chinese)(Japan)", MACHINE_IMPERFECT_SOUND )
 GAMEX( 1985, flstoryj, flstory,  flstory,  flstory,  0, ROT180, "Taito", "The FairyLand Story (Japan)", GAME_IMPERFECT_SOUND )
 GAMEX( 1985, onna34ro, 0,        onna34ro, onna34ro, 0, ROT0,   "Taito", "Onna Sansirou - Typhoon Gal (set 1)", GAME_UNEMULATED_PROTECTION | GAME_IMPERFECT_SOUND )
 GAMEX( 1985, onna34ra, onna34ro, onna34ro, onna34ro, 0, ROT0,   "Taito", "Onna Sansirou - Typhoon Gal (set 2)", GAME_UNEMULATED_PROTECTION | GAME_IMPERFECT_SOUND )
