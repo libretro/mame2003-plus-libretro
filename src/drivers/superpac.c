fbb453fc04f51b371aa7e97ae0cd3100783dbc30
grant2258 <grant2258@users.noreply.github.com>
2018-12-07 12:34:51 +0000

    more sound tag fixes

--- src/drivers/superpac.c
+++ src/drivers/superpac.c
@@ -399,7 +399,7 @@ static MACHINE_DRIVER_START( superpac )
 	MDRV_VIDEO_UPDATE(superpac)
 
 	/* sound hardware */
-	MDRV_SOUND_ADD(NAMCO, namco_interface)
+	MDRV_SOUND_ADD(NAMCO_15XX, namco_interface)
 MACHINE_DRIVER_END
 
 
