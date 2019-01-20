aab836d2b481c5cf1ad0e59c118411ea3cf56130
markwkidd <mark@stardart.net>
2019-01-20 02:37:55 -0500

    remove conflicting UINT64 and INT64 typedefs

--- src/mame2003/osd_cpu.h
+++ src/mame2003/osd_cpu.h
@@ -57,12 +57,10 @@
 extern struct retro_perf_callback perf_cb;
 
 /* typedefs are the same as coretypes.h, although those typedefs are overridden by ifndef */
-typedef uint64_t UINT64;
 typedef uint32_t UINT32;
 typedef uint16_t UINT16;
 typedef uint8_t  UINT8;
 
-typedef int64_t  INT64;
 typedef int32_t  INT32;
 typedef int16_t  INT16;
 typedef int8_t   INT8;
