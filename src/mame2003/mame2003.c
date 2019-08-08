aa8e74a40307ca70d43f168d0e52fa323d992045
arcadez2003 <51386955+arcadez2003@users.noreply.github.com>
2019-08-08 22:42:43 +0100

    fix path issue on nds and add debuggng when files dont open (part 2)
    Code via Grant2258

--- src/mame2003/mame2003.c
+++ src/mame2003/mame2003.c
@@ -753,6 +753,7 @@ static struct retro_controller_info retropad_subdevice_ports[] = {
 
 bool retro_load_game(const struct retro_game_info *game)
 {
+  int i;
   int              driverIndex    = 0;
   int              port_index;
   char             *driver_lookup = NULL;
@@ -763,17 +764,17 @@ bool retro_load_game(const struct retro_game_info *game)
     return false;
   }
 
-  log_cb(RETRO_LOG_INFO, LOGPRE "Content path: %s.\n", game->path);
+  log_cb(RETRO_LOG_INFO, LOGPRE "Full content path %s\n", game->path);
   if(!path_is_valid(game->path))
   {
     log_cb(RETRO_LOG_ERROR, LOGPRE "Content path is not valid. Exiting!");
     return false;
   }
-
+  log_cb(RETRO_LOG_INFO, LOGPRE "Git Version %s\n",GIT_VERSION);
   driver_lookup = strdup(path_basename(game->path));
   path_remove_extension(driver_lookup);
 
-  log_cb(RETRO_LOG_INFO, LOGPRE "Content lookup name: %s.\n", driver_lookup);
+  log_cb(RETRO_LOG_INFO, LOGPRE "Content lookup name: %s\n", driver_lookup);
 
   for (driverIndex = 0; driverIndex < total_drivers; driverIndex++)
   {
@@ -782,7 +783,7 @@ bool retro_load_game(const struct retro_game_info *game)
     if ((strcasecmp(driver_lookup, needle->description) == 0)
       || (strcasecmp(driver_lookup, needle->name) == 0) )
     {
-      log_cb(RETRO_LOG_INFO, LOGPRE "Driver index counter: %d. Matched game driver: %s.\n",  driverIndex, needle->name);
+      log_cb(RETRO_LOG_INFO, LOGPRE "Driver index counter: %d. Matched game driver: %s\n",  driverIndex, needle->name);
       game_driver = needle;
       options.romset_filename_noext = driver_lookup;
       break;
@@ -801,6 +802,10 @@ bool retro_load_game(const struct retro_game_info *game)
 
   options.libretro_content_path = strdup(game->path);
   path_basedir(options.libretro_content_path);
+  /*fix trailing slash in path*/
+  for(i = 0; options.libretro_content_path[i] != '\0'; ++i); 
+  if ( options.libretro_content_path[i-1] == '/' || options.libretro_content_path[i-1]  == '\\' ) 
+   options.libretro_content_path[i-1] =0; 
 
   /* Get system directory from frontend */
   options.libretro_system_path = NULL;
