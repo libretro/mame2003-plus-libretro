8576d900ba33cf3ad0ecef4bfe5303fc17782392
arcadez2003 <51386955+arcadez2003@users.noreply.github.com>
2019-10-07 23:08:12 +0100

    n8080 update Nintendo split cont

--- src/includes/8080bw.h
+++ src/includes/8080bw.h
@@ -34,10 +34,8 @@ READ_HANDLER( desertgu_port_1_r );
 MACHINE_INIT( invaders );
 MACHINE_INIT( sstrangr );
 MACHINE_INIT( invad2ct );
-MACHINE_INIT( sheriff );
 MACHINE_INIT( gunfight );
 MACHINE_INIT( boothill );
-MACHINE_INIT( helifire );
 MACHINE_INIT( phantom2 );
 MACHINE_INIT( bowler );
 MACHINE_INIT( ballbomb );
@@ -47,22 +45,11 @@ MACHINE_INIT( schaser );
 MACHINE_INIT( polaris );
 MACHINE_INIT( clowns );
 
-WRITE_HANDLER( sheriff_sh_p2_w );
-READ_HANDLER( sheriff_sh_p1_r );
-READ_HANDLER( sheriff_sh_p2_r );
-READ_HANDLER( sheriff_sh_t0_r );
-READ_HANDLER( sheriff_sh_t1_r );
-
-WRITE_HANDLER( helifire_sh_p1_w );
-WRITE_HANDLER( helifire_sh_p2_w );
-READ_HANDLER( helifire_sh_p1_r );
 
 extern struct SN76477interface invaders_sn76477_interface;
 extern struct Samplesinterface invaders_samples_interface;
 extern struct SN76477interface invad2ct_sn76477_interface;
 extern struct Samplesinterface invad2ct_samples_interface;
-extern struct DACinterface sheriff_dac_interface;
-extern struct SN76477interface sheriff_sn76477_interface;
 extern struct Samplesinterface boothill_samples_interface;
 extern struct DACinterface schaser_dac_interface;
 extern struct CustomSound_interface schaser_custom_interface;
@@ -88,7 +75,6 @@ DRIVER_INIT( lupin3 );
 DRIVER_INIT( seawolf );
 DRIVER_INIT( blueshrk );
 DRIVER_INIT( desertgu );
-DRIVER_INIT( helifire );
 DRIVER_INIT( phantom2 );
 DRIVER_INIT( bowler );
 DRIVER_INIT( gunfight );
@@ -96,7 +82,6 @@ DRIVER_INIT( bandido );
 
 void c8080bw_flip_screen_w(int data);
 void c8080bw_screen_red_w(int data);
-void c8080bw_helifire_colors_change_w(int data);
 
 INTERRUPT_GEN( polaris_interrupt );
 INTERRUPT_GEN( phantom2_interrupt );
@@ -104,16 +89,13 @@ INTERRUPT_GEN( phantom2_interrupt );
 WRITE_HANDLER( c8080bw_videoram_w );
 WRITE_HANDLER( schaser_colorram_w );
 READ_HANDLER( schaser_colorram_r );
-WRITE_HANDLER( helifire_colorram_w );
 WRITE_HANDLER( spaceint_color_w );
 WRITE_HANDLER( cosmo_colorram_w );
 
 VIDEO_UPDATE( 8080bw );
 
-VIDEO_EOF (helifire);
 
 PALETTE_INIT( invadpt2 );
-PALETTE_INIT( helifire );
 PALETTE_INIT( sflush );
 PALETTE_INIT( cosmo );
 
