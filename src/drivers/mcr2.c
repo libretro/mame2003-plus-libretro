299309ca5dc01cfe18ffb25a87efba1f4e759b72
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2020-05-21 22:05:46 -0400

    Match volume level
    Needed to match volume levels, for stereo or mono out with pause sample feature

--- src/drivers/mcr2.c
+++ src/drivers/mcr2.c
@@ -200,6 +200,7 @@ static WRITE_HANDLER( journey_sample_select_w )
   /* start sample with looping on and pause it) */
   if (!sample_playing(0) )
   {
+    sample_set_volume(0, 100);
     sample_start(0, 0, 1);
     sample_set_pause(0, 1);
   }
