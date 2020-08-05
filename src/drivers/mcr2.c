0ef491d7b882f4fcb3690934ad808857a16a5bdd
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2020-08-05 16:53:47 -0400

    Update mcr2.c

--- src/drivers/mcr2.c
+++ src/drivers/mcr2.c
@@ -616,7 +616,7 @@ INPUT_PORTS_START( twotigra )
 	PORT_ANALOG( 0xff, 0x7f, IPT_AD_STICK_X | IPF_PLAYER1, 100, 10, 0, 255 )
 
 	PORT_START	/* IN3 -- dipswitches */
-	PORT_DIPNAME( 0x01, 0x00, "Shot" )
+	PORT_DIPNAME( 0x01, 0x00, "Shot Type" )
 	PORT_DIPSETTING(    0x01, "Single Fire" )
 	PORT_DIPSETTING(    0x00, "Rapid Fire" )
 	PORT_DIPNAME( 0x02, 0x00, "Dogfight" )
