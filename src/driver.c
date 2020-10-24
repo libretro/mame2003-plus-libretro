76b5fc13cf4f14bf6911ae62e371ce4729686795
arcadez2003 <51386955+arcadez2003@users.noreply.github.com>
2020-10-24 21:13:14 +0100

    Sexy Pariodius Asia

--- src/driver.c
+++ src/driver.c
@@ -3715,6 +3715,7 @@ BOMULEUL CHAJARA SEGA ST-V  1997/04/11
 	DRIVER( tkmmpzdm )	/* GX515 (c) 1995 (Japan) */
 	DRIVER( salmndr2 )	/* GX521 (c) 1996 (Japan) */
 	DRIVER( sexyparo )	/* GX533 (c) 1996 (Japan) */
+  DRIVER( sexyparoa )	/* GX533 (c) 1996 (Asia) */  
 	DRIVER( daiskiss )	/* GX535 (c) 1996 (Japan) */
 	DRIVER( tokkae )	/* GX615 (c) 1996 (Japan) */
 	DRIVER( winspike )	/* GX705 */
