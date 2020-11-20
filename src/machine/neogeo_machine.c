6d1138baa030b9c87fceb33527ebef17392c8441
odarknighto <zyzy8501258@gmail.com>
2020-11-20 13:28:58 +0800

    add neogeo kof bootleg game
    add neogeo kof bootleg game (kof97pls,kog,kof98k,kof2001h,cthd2003,ct2k3sp,kf2k2pls,kf2k2pla,kf2k2mp,kf2k2mp2,kof2k4se,kof2003,kf2k3bl,kf2k3bla,kf2k3pl,kf2k3upl,kf10thep,kf2k5uni)
    fix kof10th

--- src/machine/neogeo_machine.c
+++ src/machine/neogeo_machine.c
@@ -601,6 +601,11 @@ static void neogeo_custom_memory(void)
 			!strcmp(Machine->gamedrv->name,"kof96h") ||
 			!strcmp(Machine->gamedrv->name,"kof97") ||
 			!strcmp(Machine->gamedrv->name,"kof97a") ||
+			!strcmp(Machine->gamedrv->name,"kof97pls") ||
+			!strcmp(Machine->gamedrv->name,"kog") ||
+			!strcmp(Machine->gamedrv->name,"kof98") ||
+			!strcmp(Machine->gamedrv->name,"kof98n") ||
+			!strcmp(Machine->gamedrv->name,"kof98k") ||
 			!strcmp(Machine->gamedrv->name,"kof98") ||
 			!strcmp(Machine->gamedrv->name,"kof98n") ||
 			!strcmp(Machine->gamedrv->name,"kof99") ||
