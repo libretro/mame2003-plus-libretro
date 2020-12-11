28a64c65e8a3b9dcd171659a930eeb80398e25f2
arcadez2003 <51386955+arcadez2003@users.noreply.github.com>
2020-12-11 00:15:18 +0000

    Move neogeo_init call
    grant2258 fixed kof2003d in this core by doin this, so just to be on the safe side i'll shift it to the bottom of the DRIVER_INIT

--- src/drivers/neogeo.c
+++ src/drivers/neogeo.c
@@ -8897,14 +8897,15 @@ DRIVER_INIT( svc )
 	kof2000_neogeo_gfx_decrypt(0x57);
 	
 	neogeo_fix_bank_type = 2;
-	init_neogeo();
-	
+  
 	install_mem_read16_handler( 0, 0x2fe000, 0x2fffdf, MRA16_RAM );
 	install_mem_write16_handler( 0, 0x2fe000, 0x2fffdf, MWA16_RAM );
 	install_mem_read16_handler( 0, 0x2fffe0, 0x2fffef, mv0_prot_r );
 	install_mem_write16_handler( 0, 0x2fffe0, 0x2fffef, mv0_prot_w );
 	install_mem_read16_handler( 0, 0x2ffff0, 0x2fffff, mv0_bankswitch_r );
 	install_mem_write16_handler( 0, 0x2ffff0, 0x2fffff, mv0_bankswitch_w );
+  
+  init_neogeo();
 }
 /* svc Init End */
 
