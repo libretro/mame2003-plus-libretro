d97dd1e48c07f649d8d00af9988cdc758880ea6c
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2020-12-31 19:29:44 -0500

    Close

--- src/vidhrdw/cheekyms_vidhrdw.c
+++ src/vidhrdw/cheekyms_vidhrdw.c
@@ -215,5 +215,5 @@ VIDEO_UPDATE( cheekyms )
 	redraw_man = 0;
 
 	/* copy the temporary bitmap to the screen over the sprites */
-	copybitmap(bitmap,tmpbitmap,0,0,0,0,cliprect,TRANSPARENCY_NONE,0);
+	copybitmap(bitmap,tmpbitmap,0,0,0,0,cliprect,TRANSPARENCY__PEN,Machine->pens[4*char_palette]);
 }
