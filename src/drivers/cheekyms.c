79f61d26a70fb190e26ac2fd70e099b4ec0deeac
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2021-02-18 20:11:15 -0500

    100

--- src/drivers/cheekyms.c
+++ src/drivers/cheekyms.c
@@ -134,7 +134,7 @@ static struct GfxDecodeInfo gfxdecodeinfo[] =
 static struct DACinterface dac_interface =
 {
 	8,
-	{ 25, 25, 25, 25, 25, 25, 25, 25 }
+	{ 100, 100, 100, 100, 100, 100, 100, 100 }
 };
 
 
