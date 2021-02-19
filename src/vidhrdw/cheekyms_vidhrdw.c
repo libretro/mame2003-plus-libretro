9296cde9b48ba7ed0f7a1586ad5743e838f026da
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2021-02-19 05:19:08 -0500

    Loopf

--- src/vidhrdw/cheekyms_vidhrdw.c
+++ src/vidhrdw/cheekyms_vidhrdw.c
@@ -49,13 +49,12 @@ WRITE_HANDLER( cheekyms_sprite_w )
 
 WRITE_HANDLER( cheekyms_port_40_w )
 {
-	DAC_data_w(0, BIT(data, 7)); /* tune                */
-	DAC_data_w(0, BIT(data, 6)); /* mouse eating cheese */
-	DAC_data_w(0, BIT(data, 5)); /* hammer              */
-	DAC_data_w(0, BIT(data, 4)); /* mouse died          */
-	DAC_data_w(0, BIT(data, 3)); /* mystery died        */
-	DAC_data_w(0, BIT(data, 2)); /* mouse appears       */
-	DAC_data_w(0, BIT(data, 1)); /* mystery appears     */
+	int i;
+
+	for(i=0; i < 256; i++)
+	{
+		DAC_data_w(0, data & i);
+	}
 }
 
 
