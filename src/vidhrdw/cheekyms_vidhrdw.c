df5256c98e197c9abaac79040ffc5c7b0884ed0c
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2021-02-18 21:08:53 -0500

    Force

--- src/vidhrdw/cheekyms_vidhrdw.c
+++ src/vidhrdw/cheekyms_vidhrdw.c
@@ -49,14 +49,12 @@ WRITE_HANDLER( cheekyms_sprite_w )
 
 WRITE_HANDLER( cheekyms_port_40_w )
 {
-	DAC_data_w(0, data & 0x80); /* tune                */   
-	DAC_data_w(1, data & 0x40); /* mouse eating cheese */
-	DAC_data_w(2, data & 0x20); /* hammer              */
-	DAC_data_w(3, data & 0x10); /* mouse died          */
-	DAC_data_w(4, data & 0x08); /* mystery died        */
-	DAC_data_w(5, data & 0x04); /* mouse appears       */
-	DAC_data_w(6, data & 0x02); /* mystery appears     */
-	/*DAC_data_w(7, BIT(data, 0));*/
+	int i;
+
+	for(i=0; i < 256; i++)
+	{
+		DAC_data_w(0, data & i);
+	}
 }
 
 
