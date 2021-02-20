23771315fcf6589e748a4036763380e4bb20d604
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2021-02-20 10:23:50 -0500

    8 Dac for cheekyms (#982)
    8 dac

--- src/vidhrdw/cheekyms_vidhrdw.c
+++ src/vidhrdw/cheekyms_vidhrdw.c
@@ -49,12 +49,13 @@ WRITE_HANDLER( cheekyms_sprite_w )
 
 WRITE_HANDLER( cheekyms_port_40_w )
 {
-	int i;
-
-	for(i=128; i < 256; i++)
-	{
-		DAC_data_w(0, data & i);
-	}
+	DAC_data_w(0, BIT(data, 7)); /* tune                */
+	DAC_data_w(1, BIT(data, 6)); /* mouse eating cheese */
+	DAC_data_w(2, BIT(data, 5)); /* hammer              */
+	DAC_data_w(3, BIT(data, 4)); /* mouse died          */
+	DAC_data_w(4, BIT(data, 3)); /* mystery died        */
+	DAC_data_w(5, BIT(data, 2)); /* mouse appears       */
+	DAC_data_w(6, BIT(data, 1)); /* mystery appears     */
 }
 
 
