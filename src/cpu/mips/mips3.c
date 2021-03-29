f21eabb088d390d4be5db68fcfb9e80af0743271
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2021-03-28 22:17:11 -0400

    Update mips3.c

--- src/cpu/mips/mips3.c
+++ src/cpu/mips/mips3.c
@@ -351,9 +351,12 @@ static INLINE void generate_exception(int exception, int backup)
 	else
 		mips3.pc += 0x180;
 
-/* useful for tracking interrupts */
+/*
+	useful for tracking interrupts
+
 	if ((CAUSE & 0x7f) == 0)
 		log_cb(RETRO_LOG_DEBUG, LOGPRE "Took interrupt -- Cause = %08X, PC =  %08X\n", (UINT32)CAUSE, mips3.pc);
+*/
 
 	/* swap to the new space */
 	if (mips3.bigendian)
@@ -376,7 +379,7 @@ static INLINE void invalid_instruction(UINT32 op)
 
 static void check_irqs(void)
 {
-	if ((CAUSE & SR & 0xff00) && (SR & SR_IE) && !(SR & SR_EXL) && !(SR & SR_ERL))
+	if ((CAUSE & SR & 0xfc00) && (SR & SR_IE) && !(SR & SR_EXL) && !(SR & SR_ERL))
 		generate_exception(EXCEPTION_INTERRUPT, 0);
 }
 
@@ -554,38 +557,44 @@ void mips3drc_set_options(UINT8 cpunum, UINT32 opts)
 **	COP0 (SYSTEM) EXECUTION HANDLING
 **#################################################################################################*/
 
-static UINT32 update_cycle_counting(void)
+static void update_cycle_counting(void)
 {
-	UINT32 count = (activecpu_gettotalcycles64() - mips3.count_zero_time) / 2;
-	UINT32 compare = mips3.cpr[0][COP0_Compare];
-	UINT32 cyclesleft = compare - count;
-	double newtime;
-
-/*printf("Update: count=%08X  compare=%08X  delta=%08X  SR=%08X  time=%f\n", count, compare, cyclesleft, (UINT32)SR, TIME_IN_CYCLES(((UINT64)cyclesleft * 2), cpu_getactivecpu()));*/
-
 	/* modify the timer to go off */
-	newtime = TIME_IN_CYCLES(((UINT64)cyclesleft * 2), cpu_getactivecpu());
-	if (SR & 0x8000)
-		timer_adjust(mips3.compare_int_timer, newtime, cpu_getactivecpu(), 0);
+	if ((SR & 0x8000) && mips3.cpr[0][COP0_Compare] != 0xffffffff)
+	{
+		UINT32 count = (activecpu_gettotalcycles64() - mips3.count_zero_time) / 2;
+		UINT32 compare = mips3.cpr[0][COP0_Compare];
+		UINT32 cyclesleft = compare - count;
+		double newtime = TIME_IN_CYCLES(((UINT64)cyclesleft * 2), cpu_getactivecpu());
+		
+		/* due to accuracy issues, don't bother setting timers unless they're for less than 100msec */
+		if (newtime < TIME_IN_MSEC(100))
+			timer_adjust(mips3.compare_int_timer, newtime, cpu_getactivecpu(), 0);
+	}
 	else
 		timer_adjust(mips3.compare_int_timer, TIME_NEVER, cpu_getactivecpu(), 0);
-	return count;
 }
 
 static INLINE UINT64 get_cop0_reg(int idx)
 {
 	if (idx == COP0_Count)
 	{
-		/* it doesn't really take 100 cycles to read this register, but it helps speed */
+		/* it doesn't really take 25 cycles to read this register, but it helps speed */
 		/* up loops that hammer on it */
-		mips3_icount -= 100;
+		if (mips3_icount >= 25)
+			mips3_icount -= 25;
+		else
+			mips3_icount = 0;
 		return (UINT32)((activecpu_gettotalcycles64() - mips3.count_zero_time) / 2);
 	}
 	else if (idx == COP0_Cause)
 	{
-		/* it doesn't really take 100 cycles to read this register, but it helps speed */
+		/* it doesn't really take 25 cycles to read this register, but it helps speed */
 		/* up loops that hammer on it */
-		mips3_icount -= 100;
+		if (mips3_icount >= 25)
+			mips3_icount -= 25;
+		else
+			mips3_icount = 0;
 	}
 	return mips3.cpr[0][idx];
 }
@@ -596,8 +605,16 @@ static INLINE void set_cop0_reg(int idx, UINT64 val)
 	{
 		case COP0_Cause:
 			CAUSE = (CAUSE & 0xfc00) | (val & ~0xfc00);
-			/* update interrupts -- software ints can occur this way */
-			check_irqs();
+			if (CAUSE & 0x300)
+			{
+				/* if we're in a delay slot, propogate the target PC before generating the exception */
+				if (mips3.nextpc != ~0)
+				{
+					mips3.pc = mips3.nextpc;
+					mips3.nextpc = ~0;
+				}
+				generate_exception(EXCEPTION_INTERRUPT, 0);
+			}
 			break;
 
 		case COP0_Status:
@@ -1255,8 +1272,6 @@ static INLINE void handle_cop2(UINT32 op)
 
 int mips3_execute(int cycles)
 {
-/*printf("mips3_execute (PC=%08X)\n", mips3.pc);*/
-
 	/* count cycles and interrupt cycles */
 	mips3_icount = cycles;
 	mips3_icount -= mips3.interrupt_cycles;
@@ -1266,6 +1281,9 @@ int mips3_execute(int cycles)
 		change_pc32bedw(mips3.pc);
 	else
 		change_pc32bedw(mips3.pc);
+
+	/* update timers & such */
+	update_cycle_counting();
 
 	/* check for IRQs */
 	check_irqs();
@@ -1328,11 +1346,13 @@ int mips3_execute(int cycles)
 						temp64 = (INT64)(INT32)RSVAL32 * (INT64)(INT32)RTVAL32;
 						LOVAL64 = (INT32)temp64;
 						HIVAL64 = (INT32)(temp64 >> 32);
+						mips3_icount -= 3;
 						break;
 					case 0x19:	/* MULTU */
 						temp64 = (UINT64)RSVAL32 * (UINT64)RTVAL32;
 						LOVAL64 = (INT32)temp64;
 						HIVAL64 = (INT32)(temp64 >> 32);
+						mips3_icount -= 3;
 						break;
 					case 0x1a:	/* DIV */
 						if (RTVAL32)
@@ -1340,6 +1360,7 @@ int mips3_execute(int cycles)
 							LOVAL64 = (INT32)((INT32)RSVAL32 / (INT32)RTVAL32);
 							HIVAL64 = (INT32)((INT32)RSVAL32 % (INT32)RTVAL32);
 						}
+						mips3_icount -= 35;
 						break;
 					case 0x1b:	/* DIVU */
 						if (RTVAL32)
@@ -1347,16 +1368,19 @@ int mips3_execute(int cycles)
 							LOVAL64 = (INT32)(RSVAL32 / RTVAL32);
 							HIVAL64 = (INT32)(RSVAL32 % RTVAL32);
 						}
+						mips3_icount -= 35;
 						break;
 					case 0x1c:	/* DMULT */
 						temp64 = (INT64)RSVAL64 * (INT64)RTVAL64;
 						LOVAL64 = temp64;
 						HIVAL64 = (INT64)temp64 >> 63;
+						mips3_icount -= 7;
 						break;
 					case 0x1d:	/* DMULTU */
 						temp64 = (UINT64)RSVAL64 * (UINT64)RTVAL64;
 						LOVAL64 = temp64;
 						HIVAL64 = 0;
+						mips3_icount -= 7;
 						break;
 					case 0x1e:	/* DDIV */
 						if (RTVAL64)
@@ -1364,6 +1388,7 @@ int mips3_execute(int cycles)
 							LOVAL64 = (INT64)RSVAL64 / (INT64)RTVAL64;
 							HIVAL64 = (INT64)RSVAL64 % (INT64)RTVAL64;
 						}
+						mips3_icount -= 67;
 						break;
 					case 0x1f:	/* DDIVU */
 						if (RTVAL64)
@@ -1371,6 +1396,7 @@ int mips3_execute(int cycles)
 							LOVAL64 = RSVAL64 / RTVAL64;
 							HIVAL64 = RSVAL64 % RTVAL64;
 						}
+						mips3_icount -= 67;
 						break;
 					case 0x20:	/* ADD */
 						if (ENABLE_OVERFLOWS && RSVAL32 > ~RTVAL32) generate_exception(EXCEPTION_OVERFLOW, 1);
@@ -1505,8 +1531,6 @@ int mips3_execute(int cycles)
 
 	} while (mips3_icount > 0 || mips3.nextpc != ~0);
 
-/*printf("mips3_execute done (PC=%08X)\n", mips3.pc);*/
-
 	mips3_icount -= mips3.interrupt_cycles;
 	mips3.interrupt_cycles = 0;
 	return cycles - mips3_icount;
@@ -1527,7 +1551,7 @@ unsigned mips3_get_reg(int regnum)
 		case MIPS3_SR:		return SR;
 		case MIPS3_EPC:		return mips3.cpr[0][COP0_EPC];
 		case MIPS3_CAUSE:	return mips3.cpr[0][COP0_Cause];
-		case MIPS3_COUNT:	return mips3.cpr[0][COP0_Count];
+		case MIPS3_COUNT:	return ((activecpu_gettotalcycles64() - mips3.count_zero_time) / 2);
 		case MIPS3_COMPARE:	return mips3.cpr[0][COP0_Compare];
 
 		case MIPS3_R0:		return (UINT32)mips3.r[0];
