aace87d3d15aa040986c9ad68dacb48b1f8d4bf1
arcadez2003 <51386955+arcadez2003@users.noreply.github.com>
2021-04-18 22:59:33 +0100

    Add a working bootleg
    MAME WIP
    
    0.153: Osso added missing DAC to Cycle Shooting / Bronx, fixes missing sounds.
    0.139u2: Added dipswitches 'Bonus Life', 'Difficulty', 'Infinite Bullets', 'Lives', 'Reset Damage (Cheat)', 'Service Mode' and 10x 'Unused'.
    0.92: Tomasz Slanina added clone Bronx (bootleg 1986). Fixed gfx1 rom loading. Added Light gun and 'Coin A/B' dipswitches.
    20th December 2004: Guru - Cycle Shooting (bootleg) arrived. Thanks to the good guys at the Italian MAME Forum!

--- src/drivers/nycaptor.c
+++ src/drivers/nycaptor.c
@@ -8,6 +8,7 @@
   as NY Captor, but MCU dump is missing.
 
 What's new :
+ - added bootleg - Bronx
  - Cycle Shooting added (bigger VRAM than nycpator, dfferent (unknwn yet) banking and
    gfx control , ROMs probably are wrong mapped, gfx too)
  - Sub CPU halt (cpu #0 ,$d001)
@@ -157,6 +158,7 @@ extern UINT8 *nycaptor_scrlram;
 UINT8 *nycaptor_sharedram;
 static int generic_control_reg = 0;
 static int sound_nmi_enable=0,pending_nmi=0;
+int nyc_gametype=0;
 
 
 WRITE_HANDLER( nycaptor_videoram_w );
@@ -183,7 +185,6 @@ READ_HANDLER( nycaptor_68705_portC_r );
 READ_HANDLER( nycaptor_68705_portB_r );
 READ_HANDLER( nycaptor_68705_portA_r );
 READ_HANDLER( nycaptor_videoram_r );
-
 
 
 static WRITE_HANDLER( sub_cpu_halt_w )
@@ -222,7 +223,10 @@ static READ_HANDLER( nycaptor_b_r )
 
 static READ_HANDLER( nycaptor_by_r )
 {
-		return readinputport(6)-8;
+	int port=readinputport(6);
+	if(nyc_gametype==1)
+			port=255-port;
+		return port-8;
 }
 
 static READ_HANDLER( nycaptor_bx_r )
@@ -282,6 +286,11 @@ static WRITE_HANDLER( nmi_enable_w )
 	}
 }
 
+static WRITE_HANDLER(unk_w)
+{
+
+}
+
 static struct AY8910interface ay8910_interface =
 {
 	2,
@@ -289,8 +298,8 @@ static struct AY8910interface ay8910_interface =
 	{ 15, 15 },
 	{ 0,0 },
 	{ 0,0 },
-	{ 0,0 },
-	{ 0,0 }
+	{ unk_w,unk_w },
+	{ unk_w,unk_w }
 };
 
 static struct MSM5232interface msm5232_interface =
@@ -439,17 +448,36 @@ static READ_HANDLER(cyclshtg_mcu_r)
   return 7;
 }
 
-static WRITE_HANDLER(cyclshtg_mcu_w){}
+static WRITE_HANDLER(cyclshtg_mcu_w)
+{
 
+}
 
+static READ_HANDLER(cyclshtg_mcu_status_r1)
+{
+  return rand();
+}
+
+static WRITE_HANDLER( cyclshtg_generic_control_w )
+{
+	int bank=(data>>2)&3;
+	generic_control_reg = data;
+	cpu_setbank(1, memory_region(REGION_CPU1) + 0x10000 + bank*0x4000 );
+}
+
+static struct DACinterface dac_interface =
+{
+	1,
+	{ 50 }
+};
 
 static MEMORY_READ_START( cyclshtg_readmem )
-  { 0x0000, 0x7fff, MRA_ROM },
+    { 0x0000, 0x7fff, MRA_ROM },
 	{ 0x8000, 0xbfff, MRA_BANK1 },
-  { 0xc000, 0xcfff, nycaptor_videoram_r },
-  { 0xd000, 0xd000, cyclshtg_mcu_r },
+    { 0xc000, 0xcfff, nycaptor_videoram_r },
+    { 0xd000, 0xd000, cyclshtg_mcu_r },
 	{ 0xd002, 0xd002, nycaptor_generic_control_r },
-  { 0xd400, 0xd400, from_snd_r },
+    { 0xd400, 0xd400, from_snd_r },
 	{ 0xd800, 0xd800, input_port_0_r },
 	{ 0xd801, 0xd801, input_port_1_r },
 	{ 0xd802, 0xd802, input_port_2_r },
@@ -462,7 +490,7 @@ static MEMORY_READ_START( cyclshtg_readmem )
 	{ 0xdca0, 0xdcbf, nycaptor_scrlram_r },
 	{ 0xdd00, 0xdeff, nycaptor_palette_r },
 	{ 0xdf03, 0xdf03, nycaptor_gfxctrl_r },
-  { 0xe000, 0xffff, nycaptor_sharedram_r },
+    { 0xe000, 0xffff, nycaptor_sharedram_r },
 MEMORY_END
 
 static MEMORY_WRITE_START( cyclshtg_writemem )
@@ -470,14 +498,14 @@ static MEMORY_WRITE_START( cyclshtg_writemem )
 	{ 0xc000, 0xcfff, nycaptor_videoram_w, &videoram, &videoram_size },
 	{ 0xd000, 0xd000, cyclshtg_mcu_w },
 	{ 0xd001, 0xd001, sub_cpu_halt_w },
-	{ 0xd002, 0xd002, nycaptor_generic_control_w },
+	{ 0xd002, 0xd002, cyclshtg_generic_control_w },
 	{ 0xd400, 0xd400, sound_command_w },
 	{ 0xd403, 0xd403, sound_cpu_reset_w },
 	{ 0xdc00, 0xdc9f, nycaptor_spriteram_w},
 	{ 0xdca0, 0xdcbf, nycaptor_scrlram_w, &nycaptor_scrlram },
 	{ 0xdce1, 0xdce1, MWA_NOP},
 	{ 0xdd00, 0xdeff, nycaptor_palette_w },
-	/*{ 0xdf03, 0xdf03, nycaptor_gfxctrl_w },*/
+	{ 0xdf03, 0xdf03, nycaptor_gfxctrl_w },
 	{ 0xe000, 0xffff, nycaptor_sharedram_w,&nycaptor_sharedram },
 MEMORY_END
 
@@ -508,47 +536,180 @@ static MEMORY_WRITE_START( cyclshtg_writemem_sub )
 	{ 0xe000, 0xffff, nycaptor_sharedram_w },
 MEMORY_END
 
+static READ_HANDLER(unk_r)
+{
+  return rand();
+}
 
-/* Cycle Shooting */
-INPUT_PORTS_START( cyclshtg )
+static MEMORY_READ_START( bronx_readmem )
+    { 0x0000, 0x7fff, MRA_ROM },
+    { 0x8000, 0xbfff, MRA_BANK1 },
+    { 0xc000, 0xcfff, nycaptor_videoram_r },
+    { 0xd000, 0xd000, cyclshtg_mcu_r },
+    { 0xd002, 0xd002, nycaptor_generic_control_r },
+    { 0xd400, 0xd400, from_snd_r },
+    { 0xd401, 0xd401, unk_r },
+    { 0xd800, 0xd800, input_port_0_r },
+	{ 0xd801, 0xd801, input_port_1_r },
+	{ 0xd802, 0xd802, input_port_2_r },
+	{ 0xd803, 0xd803, input_port_3_r },
+	{ 0xd804, 0xd804, input_port_4_r },
+	{ 0xd805, 0xd805, cyclshtg_mcu_status_r },
+	{ 0xd806, 0xd806, MRA_NOP },
+	{ 0xd807, 0xd807, cyclshtg_mcu_status_r },
+	{ 0xdc00, 0xdc9f, nycaptor_spriteram_r },
+	{ 0xdca0, 0xdcbf, nycaptor_scrlram_r },
+	{ 0xdd00, 0xdeff, nycaptor_palette_r },
+	{ 0xdf03, 0xdf03, nycaptor_gfxctrl_r },
+    { 0xe000, 0xffff, nycaptor_sharedram_r },
+MEMORY_END
+
+static MEMORY_WRITE_START( bronx_writemem )
+    { 0x0000, 0xbfff, MWA_ROM },
+	{ 0xc000, 0xcfff, nycaptor_videoram_w, &videoram, &videoram_size },
+	{ 0xd000, 0xd000, MWA_NOP },
+	{ 0xd001, 0xd001, sub_cpu_halt_w },
+	{ 0xd002, 0xd002, cyclshtg_generic_control_w },
+	{ 0xd400, 0xd400, sound_command_w },
+	{ 0xd403, 0xd403, sound_cpu_reset_w },
+	{ 0xdc00, 0xdc9f, nycaptor_spriteram_w },
+	{ 0xdca0, 0xdcbf, nycaptor_scrlram_w, &nycaptor_scrlram },
+	{ 0xdd00, 0xdeff, nycaptor_palette_w },
+    { 0xdf03, 0xdf03, nycaptor_gfxctrl_w },
+    { 0xe000, 0xffff, nycaptor_sharedram_w, &nycaptor_sharedram },
+MEMORY_END
+
+static MEMORY_READ_START( bronx_readmem_sub )
+    { 0x0000, 0x7fff, MRA_ROM },
+    { 0xd800, 0xd800, input_port_0_r },
+    { 0xd801, 0xd801, input_port_1_r },
+	{ 0xd802, 0xd802, input_port_2_r },
+	{ 0xd803, 0xd803, input_port_3_r },
+	{ 0xd804, 0xd804, input_port_4_r },
+	{ 0xd805, 0xd805, cyclshtg_mcu_status_r1 },
+	{ 0xd807, 0xd807, cyclshtg_mcu_status_r },
+	{ 0xdc00, 0xdc9f, nycaptor_spriteram_r },
+	{ 0xdd00, 0xdeff, nycaptor_palette_r },
+	{ 0xdf00, 0xdf00, nycaptor_bx_r },
+	{ 0xdf01, 0xdf01, nycaptor_by_r },
+	{ 0xdf02, 0xdf02, nycaptor_b_r },
+	{ 0xdf03, 0xdf03, nycaptor_gfxctrl_r },
+    { 0xe000, 0xffff, nycaptor_sharedram_r },
+MEMORY_END
+
+static MEMORY_WRITE_START( bronx_writemem_sub )
+    { 0x0000, 0xbfff, MWA_ROM },
+	{ 0xc000, 0xc7ff, nycaptor_videoram_w, &videoram, &videoram_size },
+	{ 0xdc00, 0xdc9f, nycaptor_spriteram_w },
+	{ 0xdca0, 0xdcbf, nycaptor_scrlram_w, &nycaptor_scrlram },
+	{ 0xdd00, 0xdeff, nycaptor_palette_w },
+	{ 0xdf03, 0xdf03, nycaptor_gfxctrl_w },
+	{ 0xe000, 0xffff, nycaptor_sharedram_w },
+MEMORY_END
+
+static READ_HANDLER( port_fetch_r )
+{
+	return memory_region(REGION_USER1)[offset];
+}
+
+static PORT_READ_START( bronx_readport )
+	{ 0x0000, 0x7fff, port_fetch_r },
+PORT_END
 
 
+static MEMORY_WRITE_START( bronx_sound_writemem )
+	{ 0x0000, 0xbfff, MWA_ROM },
+	{ 0xc000, 0xc7ff, MWA_RAM },
+	{ 0xc800, 0xc800, AY8910_control_port_0_w },
+	{ 0xc801, 0xc801, AY8910_write_port_0_w },
+	{ 0xc802, 0xc802, AY8910_control_port_1_w },
+	{ 0xc803, 0xc803, AY8910_write_port_1_w },
+	{ 0xc900, 0xc90d, MSM5232_0_w },
+	{ 0xca00, 0xca00, MWA_NOP},
+	{ 0xcb00, 0xcb00, MWA_NOP},
+	{ 0xcc00, 0xcc00, MWA_NOP},
+	{ 0xd000, 0xd000, to_main_w },
+	{ 0xd200, 0xd200, nmi_enable_w },
+	{ 0xd400, 0xd400, nmi_disable_w },
+	{ 0xd600, 0xd600, DAC_0_signed_data_w },
+	{ 0xe000, 0xefff, MWA_NOP },
+MEMORY_END
+
+
+/* verified from Z80 code */
+INPUT_PORTS_START( bronx )
+	PORT_START
+	PORT_BIT( 0x01, IP_ACTIVE_LOW, IPT_UNUSED )
+	PORT_BIT( 0x02, IP_ACTIVE_LOW, IPT_UNUSED )
+	PORT_SERVICE( 0x04, IP_ACTIVE_LOW ) 
+	PORT_DIPNAME( 0x08, 0x08, DEF_STR( Demo_Sounds ) ) 
+	PORT_DIPSETTING(    0x00, DEF_STR( Off ) ) 
+	PORT_DIPSETTING(    0x08, DEF_STR( On ) )
+	PORT_DIPNAME( 0x30, 0x30, DEF_STR( Coin_A ) ) 
+	PORT_DIPSETTING(    0x10, DEF_STR( 2C_1C ) ) 
+	PORT_DIPSETTING(    0x30, DEF_STR( 1C_1C ) ) 
+	PORT_DIPSETTING(    0x00, DEF_STR( 2C_3C ) ) 
+	PORT_DIPSETTING(    0x20, DEF_STR( 1C_2C ) ) 
+	/* coinage B isn't mentionned in the manual */
+	PORT_DIPNAME( 0xc0, 0xc0, DEF_STR( Coin_B ) ) 
+	PORT_DIPSETTING(    0x40, DEF_STR( 2C_1C ) ) 
+	PORT_DIPSETTING(    0xc0, DEF_STR( 1C_1C ) ) 
+	PORT_DIPSETTING(    0x00, DEF_STR( 2C_3C ) ) 
+	PORT_DIPSETTING(    0x80, DEF_STR( 1C_2C ) )
 
 	PORT_START
- 	  PORT_DIPNAME( 0x04, 0x04, "Test Mode" )
-  	PORT_DIPSETTING(    0x04, DEF_STR( Off ) )
- 	  PORT_DIPSETTING(    0x00, DEF_STR( On ) )
- 	  PORT_DIPNAME( 0x08, 0x08, DEF_STR( Demo_Sounds ) )
- 	  PORT_DIPSETTING(    0x00, DEF_STR( Off ) )
-  	PORT_DIPSETTING(    0x08, DEF_STR( On ) )
+    PORT_DIPNAME( 0x03, 0x03, DEF_STR( Difficulty ) ) 
+	PORT_DIPSETTING(    0x02, "Easy" ) 
+	PORT_DIPSETTING(    0x03, "Medium" ) 
+	PORT_DIPSETTING(    0x01, "Hard" ) 
+	PORT_DIPSETTING(    0x00, "Hardest" )
+	PORT_DIPNAME( 0x0c, 0x08, DEF_STR( Bonus_Life ) )       /* table at 0x100f - see notes for 'bronx' */
+	PORT_DIPSETTING(	0x0c, "150k 350k 200k+" )
+	PORT_DIPSETTING(	0x08, "200k 500k 300k+" )
+	PORT_DIPSETTING(	0x04, "300k 700k 400k+" )
+	PORT_DIPSETTING(	0x00, "400k 900k 500k+" )
+	PORT_DIPNAME( 0x30, 0x30, DEF_STR( Lives ) )            /* see notes */
+	PORT_DIPSETTING(	0x00, "1" )
+	PORT_DIPSETTING(	0x30, "2" )
+	PORT_DIPSETTING(	0x10, "4" )
+	PORT_DIPSETTING(	0x20, "5" )
+	PORT_BIT( 0x40, IP_ACTIVE_LOW, IPT_UNUSED )
+	PORT_DIPNAME( 0x80, 0x80, "Reset Damage (Cheat)" )      /* see notes */
+	PORT_DIPSETTING(	0x80, DEF_STR( Off ) )
+	PORT_DIPSETTING(	0x00, DEF_STR( On ) )
 
+	PORT_START
+	PORT_BIT( 0x01, IP_ACTIVE_LOW, IPT_UNUSED )
+	PORT_BIT( 0x02, IP_ACTIVE_LOW, IPT_UNUSED )
+	PORT_BIT( 0x04, IP_ACTIVE_LOW, IPT_UNUSED )
+	PORT_BIT( 0x08, IP_ACTIVE_LOW, IPT_UNUSED )
+	PORT_DIPNAME( 0x10, 0x10, "Infinite Bullets" )          /* see notes */
+	PORT_DIPSETTING(	0x10, DEF_STR( Off ) )
+	PORT_DIPSETTING(	0x00, DEF_STR( On ) )
+	PORT_BIT( 0x20, IP_ACTIVE_LOW, IPT_UNUSED )
+	PORT_BIT( 0x40, IP_ACTIVE_LOW, IPT_UNUSED )
+	PORT_BIT( 0x80, IP_ACTIVE_LOW, IPT_UNUSED )
 
- PORT_START
+	PORT_START
+	PORT_BIT( 0x01, IP_ACTIVE_LOW, IPT_START1 )
+	PORT_BIT( 0x02, IP_ACTIVE_LOW, IPT_UNKNOWN )            /* IPT_START2 is some similar Taito games (eg: 'flstory') */
+	PORT_BIT( 0x04, IP_ACTIVE_LOW, IPT_SERVICE1 )
+	PORT_BIT( 0x08, IP_ACTIVE_LOW, IPT_TILT )
+	PORT_BIT( 0x10, IP_ACTIVE_HIGH, IPT_COIN1 )
+	PORT_BIT( 0x20, IP_ACTIVE_HIGH, IPT_COIN2 )
+	PORT_BIT( 0x40, IP_ACTIVE_LOW, IPT_UNKNOWN )
+	PORT_BIT( 0x80, IP_ACTIVE_LOW, IPT_UNKNOWN )
 
- PORT_START
+	PORT_START
+	PORT_BIT( 0x01, IP_ACTIVE_LOW, IPT_BUTTON1 )
+	PORT_BIT( 0xfe, IP_ACTIVE_LOW, IPT_UNUSED )
 
- PORT_START
+ 	PORT_START
+	PORT_ANALOG( 0xff, 0x80, IPT_LIGHTGUN_Y | IPF_PLAYER1, 25, 15, 0x00, 0xff)
 
-    PORT_BIT( 0x01, IP_ACTIVE_LOW, IPT_START1 )
-	  PORT_BIT( 0x02, IP_ACTIVE_LOW, IPT_START2 )/*?*/
-	  PORT_BIT( 0x04, IP_ACTIVE_LOW, IPT_SERVICE1 )
-    PORT_DIPNAME( 0x08, 0x08, "Tilt" )
-	  PORT_DIPSETTING(    0x08, DEF_STR( No ) )
-	  PORT_DIPSETTING(    0x00, DEF_STR( Yes ) )
-    PORT_BIT( 0x10, IP_ACTIVE_HIGH, IPT_COIN1 )
-	  PORT_BIT( 0x20, IP_ACTIVE_HIGH, IPT_COIN2 )
-	  PORT_BIT( 0x40, IP_ACTIVE_LOW, IPT_UNUSED )
-	  PORT_BIT( 0x80, IP_ACTIVE_LOW, IPT_UNUSED )
-
- PORT_START
-    PORT_BIT( 0x01, IP_ACTIVE_LOW, IPT_BUTTON1 )
-
- PORT_START
-
- PORT_START
-
+	PORT_START
+	PORT_ANALOG( 0xff, 0x80, IPT_LIGHTGUN_X | IPF_PLAYER1, 25, 15, 0x00, 0xff)
 INPUT_PORTS_END
-
 
 
 INPUT_PORTS_START( nycaptor )
@@ -690,8 +851,6 @@ static struct GfxDecodeInfo gfxdecodeinfo[] =
 };
 
 
-
-
 static MACHINE_DRIVER_START( nycaptor )
 	/* basic machine hardware */
 	MDRV_CPU_ADD(Z80,8000000/2)		/* ??? */
@@ -744,7 +903,7 @@ static MACHINE_DRIVER_START( cyclshtg )
 
 	MDRV_CPU_ADD(Z80,8000000/2)
 	MDRV_CPU_FLAGS(CPU_AUDIO_CPU)
-	MDRV_CPU_MEMORY(sound_readmem,sound_writemem)
+	MDRV_CPU_MEMORY(sound_readmem,bronx_sound_writemem)
 	MDRV_CPU_VBLANK_INT(irq0_line_hold,2)
 
 #ifdef USE_MCU
@@ -767,8 +926,45 @@ static MACHINE_DRIVER_START( cyclshtg )
 
 	MDRV_SOUND_ADD(AY8910, ay8910_interface)
 	MDRV_SOUND_ADD(MSM5232, msm5232_interface)
+	MDRV_SOUND_ADD(DAC, dac_interface)
 MACHINE_DRIVER_END
 
+
+static MACHINE_DRIVER_START( bronx )
+	MDRV_CPU_ADD(Z80,8000000/2)
+
+	MDRV_CPU_MEMORY(bronx_readmem, bronx_writemem)
+	MDRV_CPU_VBLANK_INT(irq0_line_hold,1)
+
+	MDRV_CPU_ADD(Z80,8000000/2)
+	MDRV_CPU_MEMORY(bronx_readmem_sub, bronx_writemem_sub)
+	MDRV_CPU_PORTS(bronx_readport,0)
+	MDRV_CPU_FLAGS(CPU_16BIT_PORT)
+
+	MDRV_CPU_VBLANK_INT(irq0_line_hold,1)
+
+	MDRV_CPU_ADD(Z80,8000000/2)
+	MDRV_CPU_FLAGS(CPU_AUDIO_CPU)
+	MDRV_CPU_MEMORY(sound_readmem,bronx_sound_writemem)
+	MDRV_CPU_VBLANK_INT(irq0_line_hold,2)
+
+	MDRV_FRAMES_PER_SECOND(60)
+	MDRV_VBLANK_DURATION(DEFAULT_60HZ_VBLANK_DURATION)
+	MDRV_INTERLEAVE(2)
+	MDRV_MACHINE_INIT(ta7630)
+	MDRV_VIDEO_ATTRIBUTES(VIDEO_TYPE_RASTER)
+	MDRV_SCREEN_SIZE(32*8, 32*8)
+	MDRV_VISIBLE_AREA(0*8, 32*8-1, 2*8, 30*8-1)
+	MDRV_GFXDECODE(gfxdecodeinfo)
+	MDRV_PALETTE_LENGTH(512)
+
+	MDRV_VIDEO_START(nycaptor)
+	MDRV_VIDEO_UPDATE(nycaptor)
+
+	MDRV_SOUND_ADD(AY8910, ay8910_interface)
+	MDRV_SOUND_ADD(MSM5232, msm5232_interface)
+	MDRV_SOUND_ADD(DAC, dac_interface)
+MACHINE_DRIVER_END
 
 
 
@@ -825,17 +1021,74 @@ ROM_START( cyclshtg )
 	ROM_LOAD( "a80_18",       0x0000, 0x0800, NO_DUMP ) /* Missing */
 
 	ROM_REGION( 0x20000, REGION_GFX1, ROMREGION_DISPOSE | ROMREGION_INVERT )
+	ROM_LOAD( "a80_11.u11",   0x00000, 0x4000, CRC(29e1293b) SHA1(106204ec46fae5d3b5e4a4d423bc0e886a637c61) )
+	ROM_LOAD( "a80_10.u10",   0x04000, 0x4000, CRC(345f576c) SHA1(fee5b2167bcd0fdc21c0a7b22ffdf7506a24baee) )
+	ROM_LOAD( "a80_09.u9",    0x08000, 0x4000, CRC(3ef06dff) SHA1(99bbd32ae89a6becac9e1bb8a34a834d81890444) )
+	ROM_LOAD( "a97_07.u8",    0x0c000, 0x4000, CRC(8f2baf57) SHA1(bb4e5b69477c51536dfd95ea59c7281159f3bcc7) )
+	ROM_LOAD( "a80_15.u39",   0x10000, 0x4000, CRC(2cefb47d) SHA1(3bef20c9c0c4f9237a327da3cbc9a7bbf63771ea) )
+	ROM_LOAD( "a80_14.u34",   0x14000, 0x4000, CRC(91642de8) SHA1(531974fc147d25e9feada89bc82d5df62ec9d446) )
+	ROM_LOAD( "a80_13.u33",   0x18000, 0x4000, CRC(96a67c6b) SHA1(3bed4313d6b8d554b35abb1515ab94e78c2718c5) )
+	ROM_LOAD( "a80_12.u23",   0x1c000, 0x4000, CRC(9ff04c85) SHA1(a5edc50bbe6e2c976895c97400c75088bc90a1fc) )
+ROM_END
 
-	ROM_LOAD( "a97_07.u8",    0x00000, 0x4000, CRC(8f2baf57) SHA1(bb4e5b69477c51536dfd95ea59c7281159f3bcc7) )
-	ROM_LOAD( "a80_09.u9",    0x04000, 0x4000, CRC(3ef06dff) SHA1(99bbd32ae89a6becac9e1bb8a34a834d81890444) )
-	ROM_LOAD( "a80_10.u10",   0x08000, 0x4000, CRC(345f576c) SHA1(fee5b2167bcd0fdc21c0a7b22ffdf7506a24baee) )
-	ROM_LOAD( "a80_11.u11",   0x0c000, 0x4000, CRC(29e1293b) SHA1(106204ec46fae5d3b5e4a4d423bc0e886a637c61) )
-	ROM_LOAD( "a80_12.u23",   0x10000, 0x4000, CRC(9ff04c85) SHA1(a5edc50bbe6e2c976895c97400c75088bc90a1fc) )
-	ROM_LOAD( "a80_13.u33",   0x14000, 0x4000, CRC(96a67c6b) SHA1(3bed4313d6b8d554b35abb1515ab94e78c2718c5) )
-	ROM_LOAD( "a80_14.u34",   0x18000, 0x4000, CRC(91642de8) SHA1(531974fc147d25e9feada89bc82d5df62ec9d446) )
-	ROM_LOAD( "a80_15.u39",   0x1c000, 0x4000, CRC(2cefb47d) SHA1(3bef20c9c0c4f9237a327da3cbc9a7bbf63771ea) )
+/*
+Bronx (Cycle Shooting bootleg)
+Taito, 1986
+
+The hardware is an almost exact copy of the original Taito hardware,
+minus the 68705 Microcontroller :-)
+*/
+
+ROM_START( bronx )
+	ROM_REGION( 0x20000, REGION_CPU1, 0 )
+	ROM_LOAD( "1.bin",   0x00000, 0x4000, CRC(399b5063) SHA1(286e0d1a0c1a41060e87f2dadf199f38c787d40f) )
+	ROM_LOAD( "2.bin",   0x04000, 0x4000, CRC(3b5f9756) SHA1(7df0e4808799ca6a7809bd9ac3a1689a18ae1cdb) )
+	ROM_LOAD( "3.bin",   0x10000, 0x8000, CRC(d2ffd3ce) SHA1(06d237a4aa46e37192bd94e2db361c62c35d469c) )
+	ROM_LOAD( "4.bin",   0x18000, 0x8000, CRC(20cf148d) SHA1(49f49f9e58d7aa5690ff828b746ab856c71b0d9c) )
+
+	ROM_REGION( 0x10000, REGION_CPU2, 0 )
+	ROM_LOAD( "5.bin",   0x0000, 0x4000, CRC(19a1c665) SHA1(38f5f47f22740a75cf0ce6d0406368c2c86e0021) )
+	ROM_LOAD( "6.bin",   0x4000, 0x4000, CRC(2769c5ab) SHA1(b8f5a4a8c70c8d37d5e92b37faa0e25b287b3fb2) )
+
+	ROM_REGION( 0x08000, REGION_USER1, 0 )
+	ROM_LOAD( "7.bin",   0x00000, 0x8000, CRC(463f9f62) SHA1(8e1fa8f78d230d32502422078599e9e9af889a92) )
+
+	ROM_REGION( 0x10000, REGION_CPU3, 0 )
+	ROM_LOAD( "a80_16.i26",   0x0000, 0x4000, CRC(ce171a48) SHA1(e5ae9bb22f58c8857737bc6f5317866819a4e4d1) )
+	ROM_LOAD( "a80_17.i25",   0x4000, 0x4000, CRC(a90b7bbc) SHA1(bd5c96861a59a1f84bb5032775b1c70efdb7066f) )
+
+	ROM_REGION( 0x20000, REGION_GFX1, ROMREGION_DISPOSE | ROMREGION_INVERT )
+	ROM_LOAD( "a80_11.u11",   0x00000, 0x4000, CRC(29e1293b) SHA1(106204ec46fae5d3b5e4a4d423bc0e886a637c61) )
+	ROM_LOAD( "a80_10.u10",   0x04000, 0x4000, CRC(345f576c) SHA1(fee5b2167bcd0fdc21c0a7b22ffdf7506a24baee) )
+	ROM_LOAD( "a80_09.u9",    0x08000, 0x4000, CRC(3ef06dff) SHA1(99bbd32ae89a6becac9e1bb8a34a834d81890444) )
+	ROM_LOAD( "8.bin",    	  0x0c000, 0x4000, CRC(2b778d24) SHA1(caca7a18743a4bb657a7c5691d93de0ccb867003) )
+
+	ROM_LOAD( "a80_15.u39",   0x10000, 0x4000, CRC(2cefb47d) SHA1(3bef20c9c0c4f9237a327da3cbc9a7bbf63771ea) )
+	ROM_LOAD( "a80_14.u34",   0x14000, 0x4000, CRC(91642de8) SHA1(531974fc147d25e9feada89bc82d5df62ec9d446) )
+	ROM_LOAD( "a80_13.u33",   0x18000, 0x4000, CRC(96a67c6b) SHA1(3bed4313d6b8d554b35abb1515ab94e78c2718c5) )
+	ROM_LOAD( "a80_12.u23",   0x1c000, 0x4000, CRC(9ff04c85) SHA1(a5edc50bbe6e2c976895c97400c75088bc90a1fc) )
 ROM_END
 
 
-GAMEX(1985, nycaptor, 0,       nycaptor,  nycaptor, 0, ROT0,  "Taito", "N.Y. Captor", GAME_IMPERFECT_GRAPHICS | GAME_IMPERFECT_SOUND )
-GAMEX(1986, cyclshtg, 0,       cyclshtg,  cyclshtg, 0, ROT90, "Taito", "Cycle Shooting", GAME_NOT_WORKING)
+static DRIVER_INIT(bronx)
+{
+	int i;
+	for(i=0;i<0x20000;i++)
+		memory_region(REGION_CPU1)[i]=BITSWAP8(memory_region(REGION_CPU1)[i],0,1,2,3,4,5,6,7);
+	nyc_gametype=1;
+	
+}
+
+static DRIVER_INIT(nycaptor)
+{
+	nyc_gametype=0;
+}
+
+static DRIVER_INIT(cyclshtg)
+{
+	nyc_gametype=1;
+}
+
+GAMEX(1985, nycaptor, 0,        nycaptor,  nycaptor, nycaptor, ROT0,  "Taito", "N.Y. Captor", GAME_IMPERFECT_GRAPHICS | GAME_IMPERFECT_SOUND )
+GAMEX(1986, cyclshtg, 0,        cyclshtg,  bronx,    cyclshtg, ROT90, "Taito", "Cycle Shooting", GAME_NOT_WORKING )
+GAMEX(1986, bronx,    cyclshtg, bronx,     bronx,    bronx,    ROT90, "bootleg", "Bronx", GAME_IMPERFECT_GRAPHICS | GAME_IMPERFECT_SOUND )
