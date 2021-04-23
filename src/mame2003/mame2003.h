723ac10503234d67f65994f41bb31544cf0dbd56
Mark W. Kidd <mark@stardart.net>
2021-04-23 14:07:44 -0400

    refactor: less input caching

--- src/mame2003/mame2003.h
+++ src/mame2003/mame2003.h
@@ -55,17 +55,25 @@ extern "C" {
 
 ***************************************************************************/
 
-#define APPNAME         "mame2003-plus"
+#define APPNAME      "mame2003-plus"
 
-#define FRAMES_PER_FPS_UPDATE		12
-#define MAX_GFX_ELEMENTS        32
-#define MAX_MEMORY_REGIONS      32
+#define FRAMES_PER_FPS_UPDATE    12
+#define MAX_GFX_ELEMENTS         32
+#define MAX_MEMORY_REGIONS       32
 
+<<<<<<< HEAD
 #define INPUT_BUTTON_AXIS_THRESHOLD   64
 #define LIBRETRO_ANALOG_MIN -32768
 #define LIBRETRO_ANALOG_MAX 32767
 #define ANALOG_MIN -128
 #define ANALOG_MAX 128
+=======
+#define LIBRETRO_ANALOG_MIN  -32768
+#define LIBRETRO_ANALOG_MAX   32767
+#define MAME_ANALOG_MIN        -128
+#define MAME_ANALOG_MAX         128
+#define MAX_ANALOG_AXES           4
+>>>>>>> 8c2f4704 (refactor: less input caching)
 
 enum
 {
@@ -73,7 +81,6 @@ enum
   Y_AXIS,
   Z_AXIS,
   PEDAL_AXIS,
-  MAX_ANALOG_AXES
 };
 
 enum
