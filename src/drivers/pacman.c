e71430d6673cc819b13d0bd9b2a42057b9c5d7ef
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2021-04-30 15:57:49 -0400

    t6

--- src/drivers/pacman.c
+++ src/drivers/pacman.c
@@ -182,7 +182,7 @@ Dave Widel
 #include "pengo.h"
 #include "cpu/s2650/s2650.h"
 
-static data8_t *decrypted_opcodes,*decrypted_opcodes_mirror,*decrypted_opcodes_high;
+
 static UINT8 speedcheat = 0;	/* a well known hack allows to make Pac Man run at four times */
 								/* his usual speed. When we start the emulation, we check if the */
 								/* hack can be applied, and set this flag accordingly. */
@@ -986,47 +986,6 @@ static PORT_WRITE_START( s2650games_writeport )
 	{ S2650_DATA_PORT, S2650_DATA_PORT, SN76496_0_w },
 PORT_END
 
-READ_HANDLER( mspactwin_decrypted_opcodes_r )
-{
-	return decrypted_opcodes[offset];
-}
-
-WRITE_HANDLER( mspactwin_decrypted_opcodes_w )
-{
-	decrypted_opcodes[offset] = data;
-}
-
-READ_HANDLER( mspactwin_decrypted_opcodes_mirror_r )
-{
-	return decrypted_opcodes_mirror[offset];
-}
-
-WRITE_HANDLER( mspactwin_decrypted_opcodes_mirror_w )
-{
-	decrypted_opcodes_mirror[offset] = data;
-}
-
-READ_HANDLER( mspactwin_decrypted_opcodes_high_r )
-{
-	return decrypted_opcodes_high[offset];
-}
-
-WRITE_HANDLER( mspactwin_decrypted_opcodes_high_w )
-{
-	decrypted_opcodes_high[offset] = data;
-}
-
-static MEMORY_READ_START( mspactwin_decrypted_readmem )
-	{ 0x0000, 0x3fff, mspactwin_decrypted_opcodes_r },
-	//{ 0x6000, 0x7fff, mspactwin_decrypted_opcodes_mirror_r },
-	{ 0x8000, 0xbfff, mspactwin_decrypted_opcodes_high_r },
-MEMORY_END
-
-static MEMORY_WRITE_START( mspactwin_decrypted_writemem )
-	{ 0x0000, 0x3fff, mspactwin_decrypted_opcodes_w, &decrypted_opcodes },
-	//{ 0x6000, 0x7fff, mspactwin_decrypted_opcodes_mirror_w, &decrypted_opcodes_mirror },
-	{ 0x8000, 0xbfff, mspactwin_decrypted_opcodes_high_w, &decrypted_opcodes_high },
-MEMORY_END
 
 /*************************************
  *
@@ -2701,7 +2660,6 @@ static MACHINE_DRIVER_START( mspactwin )
 
 	MDRV_CPU_MODIFY("main")
 	MDRV_CPU_MEMORY(mspactwin_readmem,mspactwin_writemem)
-	//MDRV_CPU_MEMORY(mspactwin_decrypted_readmem,mspactwin_decrypted_writemem)
 	MDRV_CPU_VBLANK_INT(mspacman_interrupt,1)
 
 	MDRV_MACHINE_INIT(mspacman)
