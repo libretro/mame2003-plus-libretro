6fe550e4d4b0b05c399fa34e629ee54592ed5320
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2021-04-30 08:27:56 -0400

    operand

--- src/drivers/pacman.c
+++ src/drivers/pacman.c
@@ -4483,6 +4483,21 @@ static DRIVER_INIT( porky )
 
 }
 
+static DRIVER_INIT( mspactwin )
+{
+  UINT8 *rom = memory_region(REGION_CPU1);
+  int A;
+
+  for (int A = 0x0000; A < 0x4000; A+=2)
+  {
+    // decode operand
+    rom[       A  ] = BITSWAP8(rom[       A  ]       , 0, 1, 2, 3, 4, 5, 6, 7);
+    rom[       A+1] = BITSWAP8(rom[       A+1] ^ 0xA3, 2, 4, 6, 3, 7, 0, 5, 1);
+    rom[0x8000+A  ] = BITSWAP8(rom[0x8000+A  ]       , 0, 1, 2, 3, 4, 5, 6, 7);
+    rom[0x8000+A+1] = BITSWAP8(rom[0x8000+A+1] ^ 0xA3, 2, 4, 6, 3, 7, 0, 5, 1);
+  }
+}
+
 
 /*************************************
  *
