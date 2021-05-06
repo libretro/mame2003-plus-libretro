cba5a9c394094d760d532a49e75bb18914d61fe7
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2021-05-06 19:59:44 -0400

    Temp

--- src/vidhrdw/pengo_vidhrdw.c
+++ src/vidhrdw/pengo_vidhrdw.c
@@ -193,6 +193,14 @@ WRITE_HANDLER( pengo_flipscreen_w )
 	}
 }
 
+/*
+WRITE_HANDLER( mspactwin_flipscreen_w )
+{
+	m_flipscreen = state;
+	m_bg_tilemap->set_flip(m_flipscreen * ( TILEMAP_FLIPX + TILEMAP_FLIPY ) );
+}
+*/
+
 
 
 /***************************************************************************
