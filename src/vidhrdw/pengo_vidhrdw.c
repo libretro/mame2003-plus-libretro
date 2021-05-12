1d3f1952aa6ef01aa2ba03aabe02ca4208506915
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2021-05-12 04:27:13 -0400

    Update pengo_vidhrdw.c

--- src/vidhrdw/pengo_vidhrdw.c
+++ src/vidhrdw/pengo_vidhrdw.c
@@ -422,7 +422,7 @@ WRITE_HANDLER( mspactwin_videoram_w )
 	{
 		dirtybuffer[offset] = 1;
 		videoram[offset] = data;
-		force_partial_update(cpu_getscanline() + 16);
+		force_partial_update(cpu_getscanline() + 8);
 	}
 }
 
