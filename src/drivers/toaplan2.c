2ded60d3fe49176b9e496bd0ecc5a26d92235549
arcadez2003 <51386955+arcadez2003@users.noreply.github.com>
2021-05-21 14:46:58 +0100

    Hook up the HD647180 MCU sound support for Ghox and add a new Joytick version
    0.189: Added HD647180X MCU dumps to Ghox, allowing for sound emulation in it.  [Team CAPS0ff]. Added Z180 (10MHz) CPU2.
    0.128u2: Changed description to 'Ghox (Spinner with Up/Down Axis)' and clone (set 2) to 'Ghox (8-Way Joystick)'. Renamed (ghoxa) to (ghoxj).
    0.125u1: Tormod Tjaberg added clone Ghox (set 2). Changed parent description to 'Ghox (set 1)'.
    
    With thanks to grant2258

--- src/drivers/toaplan2.c
+++ src/drivers/toaplan2.c
@@ -180,7 +180,7 @@ Battle Bakraid			L7A0498 GP9001 TOA PLAN 9335
 Game status:
 
 Teki Paki                      Working.
-Ghox                           Working, but no sound. Missing sound MCU dump
+Ghox                           Working.
 Dogyuun                        Working, but no sound. MCU type unknown - its a Z?80 of some sort.
 Knuckle Bash                   Working, partial sound. MCU dump exists, its a Z?80 of some sort.
 Truxton 2                      Working.
@@ -337,25 +337,6 @@ data8_t m_cmdavailable;
 READ_HANDLER(tekipaki_soundlatch_r);
 READ_HANDLER(tekipaki_cmdavailable_r);
 
-/* Sample Code lifted from ThunderMAME */
-int fadeout_ready = 0;
-int fadeout_stop = 0;
-int counter1 = 0;
-float sample_vol1 = 0;
-
-int playing1 = 0xff;
-int playing2 = 0xff;
-int playing3 = 0x00;
-int playing4 = 0x00;
-int playing5 = 0x00;
-int play_bat1 = 0x00;
-
-int thunder1 = 0;
-int thunder2 = 0;
-int thunder3 = 0;
-int thunder4 = 0;
-int thunder5 = 0;
-int thunder6 = 0;
 
 /***************************************************************************
   Initialisation handlers
@@ -369,6 +350,7 @@ static MACHINE_INIT( toaplan2 )		/* machine_init_toaplan2(); */
 
 static MACHINE_INIT( ghox )
 {
+	toaplan2_shared_ram16 = auto_malloc(0xFFF);
 	old_p1_paddle_h = 0;
 	old_p1_paddle_v = 0;
 	old_p2_paddle_h = 0;
@@ -716,7 +698,7 @@ static READ16_HANDLER( ghox_p1_h_analog_r )
 	return value;
 }
 
-static READ16_HANDLER( ghox_p1_v_analog_r )
+static READ_HANDLER( ghox_p1_v_analog_r )
 {
 	INT8 new_value;
 
@@ -742,7 +724,7 @@ static READ16_HANDLER( ghox_p2_h_analog_r )
 	return value;
 }
 
-static READ16_HANDLER( ghox_p2_v_analog_r )
+static READ_HANDLER( ghox_p2_v_analog_r )
 {
 	INT8 new_value;
 
@@ -757,141 +739,6 @@ static READ16_HANDLER( ghox_p2_v_analog_r )
 	return (input_port_2_r(0) | 1);
 }
 
-static READ16_HANDLER( ghox_mcu_r )
-{
-	return 0xff;
-}
-
-static WRITE16_HANDLER( ghox_mcu_w )
-{
-	if (data == 0xfe)
-		sample_stop (0);
-
-	if (data == 0x42 || data == 0x44 || data == 0x45 || data == 0x47 || data == 0x48 || data == 0x4c || data == 0x4d || data == 0x4e)
-		sample_start (0, data , 1);
-
-	if (data == 0xd0)
-		sample_start (0, 0, 1);
-
-	if (data == 0x49)
-		sample_start (0, data , 0);
-
-	if (data >= 0x02 && data <= 0x0f)
-		sample_start (1, data , 0);
-
-	if (data >= 0x10 && data <= 0x17)
-		sample_start (2, data , 0);
-
-	if (data >= 0x18 && data <= 0x1f)
-		sample_start (3, data , 0);
-
-	if (data >= 0x20 && data <= 0x27)
-		sample_start (4, data , 0);
-
-	if (data >= 0x28 && data <= 0x2f)
-		sample_start (5, data , 0);
-
-	if (data >= 0x30 && data <= 0x38)
-		sample_start (6, data , 0);
-
-	if (data == 0x39)
-		sample_start (8, data , 0);
-
-	if (data >= 0x3a && data <= 0x3f)
-		sample_start (7, data , 0);
-
-	if (data == 0x01)
-		sample_start (8, data , 0);
-
-	if (data == 0x4b)
-		sample_start (0, 0x4f , 0);
-
-	if (ACCESSING_LSB)
-	{
-		mcu_data = data;
-		if ((data >= 0xd0) && (data < 0xe0))
-		{
-			offset = ((data & 0x0f) * 2) + (0x38 / 2);
-			toaplan2_shared_ram16[offset  ] = 0x0005;	/* Return address for */
-			toaplan2_shared_ram16[offset-1] = 0x0056;	/*   RTS instruction */
-		}
-		else
-		{
-			log_cb(RETRO_LOG_DEBUG, LOGPRE "PC:%08x Writing %08x to HD647180 cpu shared ram status port\n",activecpu_get_previouspc(),mcu_data);
-		}
-		toaplan2_shared_ram16[0x56 / 2] = 0x004e;	/* Return a RTS instruction */
-		toaplan2_shared_ram16[0x58 / 2] = 0x0075;
-
-		if (data == 0xd3)
-		{
-		toaplan2_shared_ram16[0x56 / 2] = 0x003a;	/*	move.w  d1,d5*/
-		toaplan2_shared_ram16[0x58 / 2] = 0x0001;
-		toaplan2_shared_ram16[0x5a / 2] = 0x0008;	/*	bclr.b  #0,d5*/
-		toaplan2_shared_ram16[0x5c / 2] = 0x0085;
-		toaplan2_shared_ram16[0x5e / 2] = 0x0000;
-		toaplan2_shared_ram16[0x60 / 2] = 0x0000;
-		toaplan2_shared_ram16[0x62 / 2] = 0x00cb;	/*	muls.w  #3,d5*/
-		toaplan2_shared_ram16[0x64 / 2] = 0x00fc;
-		toaplan2_shared_ram16[0x66 / 2] = 0x0000;
-		toaplan2_shared_ram16[0x68 / 2] = 0x0003;
-		toaplan2_shared_ram16[0x6a / 2] = 0x0090;	/*	sub.w   d5,d0*/
-		toaplan2_shared_ram16[0x6c / 2] = 0x0045;
-		toaplan2_shared_ram16[0x6e / 2] = 0x00e5;	/*	lsl.b   #2,d1*/
-		toaplan2_shared_ram16[0x70 / 2] = 0x0009;
-		toaplan2_shared_ram16[0x72 / 2] = 0x004e;	/*	rts*/
-		toaplan2_shared_ram16[0x74 / 2] = 0x0075;
-		}
-	}
-}
-
-static const char *ghox_sample_names[] =
-{
-	"*ghox",
-	"d0.wav","01.wav","02.wav","dm.wav","04.wav","05.wav","06.wav","dm.wav",
-	"08.wav","09.wav","dm.wav","0b.wav","0c.wav","dm.wav","dm.wav","0f.wav",
-	"dm.wav","11.wav","12.wav","12.wav","14.wav","15.wav","16.wav","17.wav",
-	"18.wav","19.wav","1a.wav","1b.wav","1c.wav","1c.wav","1c.wav","1f.wav",
-	"20.wav","21.wav","22.wav","23.wav","24.wav","dm.wav","dm.wav","27.wav",
-	"dm.wav","dm.wav","2a.wav","2b.wav","dm.wav","2d.wav","2e.wav","2f.wav",
-	"dm.wav","dm.wav","dm.wav","33.wav","34.wav","35.wav","36.wav","37.wav",
-	"38.wav","39.wav","dm.wav","dm.wav","3c.wav","dm.wav","3e.wav","dm.wav",
-	"dm.wav","dm.wav","42.wav","43.wav","44.wav","45.wav","43.wav","47.wav",
-	"48.wav","49.wav","43.wav","dm.wav","4c.wav","4d.wav","4e.wav","d1.wav",0
-};
-
-struct Samplesinterface ghox_samples_interface =
-{
-	9,
-    75,
-    ghox_sample_names
-};
-
-static READ16_HANDLER( ghox_shared_ram_r )
-{
-	/* Ghox 68K reads data from MCU shared RAM and writes it to main RAM.
-	   It then subroutine jumps to main RAM and executes this code.
-	   Here, we're just returning a RTS instruction for now.
-	   See above ghox_mcu_w routine.
-
-	   Offset $56 and $58 are accessed from around PC:0F814
-
-	   Offset $38 and $36 are accessed from around PC:0DA7C
-	   Offset $3c and $3a are accessed from around PC:02E3C
-	   Offset $40 and $3E are accessed from around PC:103EE
-	   Offset $44 and $42 are accessed from around PC:0FB52
-	   Offset $48 and $46 are accessed from around PC:06776
-	*/
-
-	return toaplan2_shared_ram16[offset] & 0xff;
-}
-
-static WRITE16_HANDLER( ghox_shared_ram_w )
-{
-	if (ACCESSING_LSB)
-	{
-		toaplan2_shared_ram16[offset] = data & 0xff;
-	}
-}
 
 /****************************************************************************
   The Toaplan 2 hardware with V25+ secondary CPU controls the sound through
@@ -1051,6 +898,15 @@ static READ16_HANDLER( shared_ram_r )
 	return toaplan2_shared_ram16[offset] & 0xff;
 }
 
+static READ_HANDLER( shared_ram8_r )
+{
+/*	Other games using a Zx80 based secondary CPU, have shared memory between
+	the 68000 and the Zx80 CPU. The 68000 reads the status of the Zx80
+	via a location of the shared memory.
+*/
+	return toaplan2_shared_ram16[offset] & 0xff;
+}
+
 static WRITE16_HANDLER( shared_ram_w )
 {
 	if (ACCESSING_LSB)
@@ -1072,6 +928,11 @@ static WRITE16_HANDLER( shared_ram_w )
 		}
 		toaplan2_shared_ram16[offset] = data;
 	}
+}
+
+static WRITE_HANDLER( shared_ram8_w )
+{
+		toaplan2_shared_ram16[offset] = data;
 }
 
 static READ16_HANDLER( Zx80_status_port_r )
@@ -1530,15 +1391,7 @@ static MEMORY_READ16_START( ghox_readmem )
 	{ 0x100000, 0x100001, ghox_p1_h_analog_r },		/* Paddle 1 */
 	{ 0x140004, 0x140007, toaplan2_0_videoram16_r },
 	{ 0x14000c, 0x14000d, toaplan2_inputport_0_word_r },	/* VBlank */
-	{ 0x180000, 0x180001, ghox_mcu_r },				/* really part of shared RAM */
-	{ 0x180006, 0x180007, input_port_4_word_r },	/* Dip Switch A */
-	{ 0x180008, 0x180009, input_port_5_word_r },	/* Dip Switch B */
-	{ 0x180010, 0x180011, input_port_3_word_r },	/* Coin/System inputs */
-/*	{ 0x18000c, 0x18000d, input_port_1_word_r },	 // Player 1 controls (real) /*/
-/*	{ 0x18000e, 0x18000f, input_port_2_word_r },	 // Player 2 controls (real) /*/
-	{ 0x18000c, 0x18000d, ghox_p1_v_analog_r },		/* Player 1 controls */
-	{ 0x18000e, 0x18000f, ghox_p2_v_analog_r },		/* Player 2 controls */
-	{ 0x180500, 0x180fff, ghox_shared_ram_r },
+	{ 0x180000, 0x180fff, shared_ram_r },
 	{ 0x18100c, 0x18100d, input_port_6_word_r },	/* Territory Jumper block */
 MEMORY_END
 
@@ -1550,8 +1403,7 @@ static MEMORY_WRITE16_START( ghox_writemem )
 	{ 0x140004, 0x140007, toaplan2_0_videoram16_w },/* Tile/Sprite VideoRAM */
 	{ 0x140008, 0x140009, toaplan2_0_scroll_reg_select_w },
 	{ 0x14000c, 0x14000d, toaplan2_0_scroll_reg_data_w },
-	{ 0x180000, 0x180001, ghox_mcu_w },				/* really part of shared RAM */
-	{ 0x180500, 0x180fff, ghox_shared_ram_w, &toaplan2_shared_ram16 },
+	{ 0x180000, 0x180fff, shared_ram_w },
 	{ 0x181000, 0x181001, toaplan2_coin_word_w },
 MEMORY_END
 
@@ -2314,6 +2166,31 @@ READ_HANDLER(tekipaki_cmdavailable_r)
 	if (m_cmdavailable) return 0xff;
 	else return 0x00;
 };
+
+/* grant2258 we initilaze shared ram at init use auto_malloc to not malloc */
+static MEMORY_READ_START( ghox_hd647180_readmem )
+    { 0x00000, 0x03fff, MRA_ROM },  /* Internal 16k byte ROM */
+	{ 0x0fe00, 0x0ffff, MRA_RAM },  /* Internal 512 byte RAM */
+	{ 0x3fe00, 0x3ffff, MRA_RAM },   /* RAM (is this actually just internal RAM getting mapped badly?) */
+	{ 0x40000, 0x4f7ff, shared_ram8_r },
+	{ 0x80002, 0x80002, input_port_4_r },
+	{ 0x80004, 0x80004, input_port_5_r },
+	{ 0x80006, 0x80006, MRA_NOP }, // nothing?
+	{ 0x80008, 0x80008, ghox_p1_v_analog_r },
+	{ 0x8000a, 0x8000a, ghox_p2_v_analog_r },
+	{ 0x8000c, 0x8000e, input_port_3_r },
+	{ 0x8000e, 0x8000f, YM2151_status_port_0_r },
+MEMORY_END
+
+
+static MEMORY_WRITE_START( ghox_hd647180_writemem )
+    { 0x00000, 0x03fff, MWA_ROM },   /* Internal 16k byte ROM */
+	{ 0x0fe00, 0x0ffff, MWA_RAM },  /* Internal 512 byte RAM */
+	{ 0x3fe00, 0x3ffff, MWA_RAM },  /* RAM (is this actually just internal RAM getting mapped badly?) */
+	{ 0x40000, 0x4f7ff, shared_ram8_w,},
+	{ 0x8000e, 0x8000f, YM2151_word_0_w },
+MEMORY_END
+
 
 static MEMORY_READ_START( hd647180_readmem )
 	{ 0x00000, 0x03fff, MRA_ROM },
@@ -4362,7 +4239,7 @@ static MACHINE_DRIVER_START( tekipaki )
 
 	MDRV_FRAMES_PER_SECOND(60)
 	MDRV_VBLANK_DURATION(DEFAULT_REAL_60HZ_VBLANK_DURATION)
-  MDRV_INTERLEAVE(10)
+    MDRV_INTERLEAVE(10)
 
 	MDRV_MACHINE_INIT(toaplan2)
 
@@ -4389,13 +4266,12 @@ static MACHINE_DRIVER_START( ghox )
 	MDRV_CPU_MEMORY(ghox_readmem,ghox_writemem)
 	MDRV_CPU_VBLANK_INT(toaplan2_vblank_irq4,262)
 
-#if HD64x180
 	MDRV_CPU_ADD(Z180, 10000000)			/* HD647180 CPU actually */
-	MDRV_CPU_MEMORY(hd647180_readmem,hd647180_writemem)
-#endif
+	MDRV_CPU_MEMORY(ghox_hd647180_readmem,ghox_hd647180_writemem)
 
 	MDRV_FRAMES_PER_SECOND(60)
 	MDRV_VBLANK_DURATION(DEFAULT_REAL_60HZ_VBLANK_DURATION)
+	MDRV_INTERLEAVE(100)
 
 	MDRV_MACHINE_INIT(ghox)
 
@@ -4411,11 +4287,7 @@ static MACHINE_DRIVER_START( ghox )
 	MDRV_VIDEO_UPDATE(toaplan2_0)
 
 	/* sound hardware */
-/*	
-	MDRV_SOUND_ATTRIBUTES(SOUND_SUPPORTS_STEREO)
 	MDRV_SOUND_ADD(YM2151, ym2151_interface)
-*/
-	MDRV_SOUND_ADD(SAMPLES, ghox_samples_interface )
 MACHINE_DRIVER_END
 
 
@@ -4970,11 +4842,21 @@ ROM_START( ghox )
 	ROM_LOAD16_BYTE( "tp021-01.u10", 0x000000, 0x020000, CRC(9e56ac67) SHA1(daf241d9e55a6e60fc004ed61f787641595b1e62) )
 	ROM_LOAD16_BYTE( "tp021-02.u11", 0x000001, 0x020000, CRC(15cac60f) SHA1(6efa3a50a5dfe6ef4072738d6a7d0d95dca8a675) )
 
-#if HD64x180
 	ROM_REGION( 0x10000, REGION_CPU2, 0 )			/* Sound HD647180 code */
-	/* sound CPU is a HD647180 (Z180) with internal ROM - not yet supported */
-	ROM_LOAD( "hd647180.021", 0x00000, 0x08000, NO_DUMP )
-#endif
+    ROM_LOAD( "hd647180.021", 0x00000, 0x08000, CRC(6ab59e5b) SHA1(d814dd3a8f1ee638794e2bd422eed4247ba4a15e) )
+
+	ROM_REGION( 0x100000, REGION_GFX1, ROMREGION_DISPOSE )
+	ROM_LOAD( "tp021-03.u36", 0x000000, 0x080000, CRC(a15d8e9d) SHA1(640a33997bdce8e84bea6a944139716379839037) )
+	ROM_LOAD( "tp021-04.u37", 0x080000, 0x080000, CRC(26ed1c9a) SHA1(37da8af86ea24327444c2d4ad3dfbd936208d43d) )
+ROM_END
+
+ROM_START( ghoxj ) /* 8-way joystick for controls */
+	ROM_REGION( 0x040000, REGION_CPU1, 0 )			/* Main 68K code */
+	ROM_LOAD16_BYTE( "tp021-01a.u10", 0x000000, 0x020000, CRC(c11b13c8) SHA1(da7defc1d3b6ddded910ba56c31fbbdb5ed57b09) )
+	ROM_LOAD16_BYTE( "tp021-02a.u11", 0x000001, 0x020000, CRC(8d426767) SHA1(1ed4a8bcbf4352257e7d58cb5c2c91eb48c2f047) )
+
+	ROM_REGION( 0x10000, REGION_CPU2, 0 )			/* Sound HD647180 code */
+    ROM_LOAD( "hd647180.021", 0x00000, 0x08000, CRC(6ab59e5b) SHA1(d814dd3a8f1ee638794e2bd422eed4247ba4a15e) )
 
 	ROM_REGION( 0x100000, REGION_GFX1, ROMREGION_DISPOSE )
 	ROM_LOAD( "tp021-03.u36", 0x000000, 0x080000, CRC(a15d8e9d) SHA1(640a33997bdce8e84bea6a944139716379839037) )
@@ -5529,7 +5411,8 @@ ROM_END
 
 /*   ( YEAR  NAME      PARENT    MACHINE   INPUT     INIT      MONITOR COMPANY    FULLNAME     FLAGS ) */
 GAME ( 1991, tekipaki, 0,        tekipaki, tekipaki, T2_Z180,  ROT0,   "Toaplan", "Teki Paki" )
-GAME ( 1991, ghox,     0,        ghox,     ghox,     T2_Z180,  ROT270, "Toaplan", "Ghox" )
+GAME ( 1991, ghox,     0,        ghox,     ghox,     T2_Z180,  ROT270, "Toaplan", "Ghox (spinner)" )
+GAME ( 1991, ghoxj,    ghox,     ghox,     ghox,     T2_Z180,  ROT270, "Toaplan", "Ghox (joystick)" )
 GAMEX( 1992, dogyuun,  0,        dogyuun,  dogyuun,  T2_Zx80,  ROT270, "Toaplan", "Dogyuun", GAME_NO_SOUND )
 GAMEX( 1993, kbash,    0,        kbash,    kbash,    T2_Zx80,  ROT0,   "Toaplan", "Knuckle Bash", GAME_IMPERFECT_SOUND )
 GAME(  1999, kbash2,   0,        kbash2,   kbash2,   T2_noZ80, ROT0,   "Toaplan", "Knuckle Bash 2" )
