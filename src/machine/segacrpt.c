bd936a860aeddc84411a6fd99611db2d647cd54c
arcadez2003 <51386955+arcadez2003@users.noreply.github.com>
2021-05-29 23:10:08 +0100

    Calorie Kun decode

--- src/machine/segacrpt.c
+++ src/machine/segacrpt.c
@@ -1346,3 +1346,5 @@ static void sega_decode_317(int order, int opcode_shift, int data_shift)
 void spcpostn_decode(void)	{ sega_decode_317( 0, 0, 1 ); }
 void gardia_decode(void)	{ sega_decode_317( 1, 1, 1 ); }
 void gardiab_decode(void)	{ sega_decode_317( 0, 1, 2 ); }
+void calorie_decode(void)	{ sega_decode_317( 1, 0, 0 ); }
+
