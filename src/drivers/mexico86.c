b8f073dce692074d82268cd04d909f00d48b39d6
arcadez2003 <51386955+arcadez2003@users.noreply.github.com>
2021-06-27 17:48:30 +0100

    Tidy up

--- src/drivers/mexico86.c
+++ src/drivers/mexico86.c
@@ -829,7 +829,7 @@ ROM_START( mexico86 )
 ROM_END
 
 
-GAME( 1986, kikikai,  0,        kikikai,  kikikai,  0,        ROT90, "Taito Corporation", "KiKi KaiKai" )
-GAME( 1986, kicknrun, 0,        kicknrun, kicknrun, 0,        ROT0,  "Taito Corporation", "Kick and Run" )
-GAME( 1986, mexico86, kicknrun, mexico86, mexico86, 0,        ROT0,  "bootleg", "Mexico 86" )
+GAME( 1986, kikikai,  0,        kikikai,  kikikai,   0,  ROT90, "Taito Corporation", "KiKi KaiKai" )
+GAME( 1986, kicknrun, 0,        kicknrun, kicknrun,  0,  ROT0,  "Taito Corporation", "Kick and Run" )
+GAME( 1986, mexico86, kicknrun, mexico86, mexico86,  0,  ROT0,  "bootleg", "Mexico 86" )
 
