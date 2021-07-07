99b96231a7e84e4302a9b4c37178a17341a531ba
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2021-07-07 11:25:56 -0400

    Update suna8.c

--- src/drivers/suna8.c
+++ src/drivers/suna8.c
@@ -1292,11 +1292,44 @@ PORT_END
 								Spark Man
 ***************************************************************************/
 
-/* Probably wrong: */
-static WRITE_HANDLER( sparkman_nmi_w )
+/*
+Thrash protection code snippet:
+
+0B48: 3E 81         ld   a,$81
+0B4A: 32 BF C3      ld   ($C3BF),a
+0B4D: 21 10 D0      ld   hl,$C808
+0B50: 11 11 D0      ld   de,$C809
+0B53: ED 5F         ld   a,r  ;check this, pretty pointless
+0B55: 77            ld   (hl),a
+0B56: 01 80 00      ld   bc,$0080
+0B59: ED B0         ldir
+0B5B: 3E 18         ld   a,$18
+0B5D: 32 C4 C3      ld   ($C3C4),a
+0B60: 21 67 13      ld   hl,$0B67
+0B63: 22 00 D0      ld   ($C800),hl
+0B66: C9            ret
+
+*/
+
+static UINT8 suna8_trash_prot, *suna8_wram;
+
+/* This is a command-based protection. */
+static WRITE_HANDLER( sparkman_cmd_prot_w )
 {
-	suna8_nmi_enable = data & 0x01;
-	if (data & ~0x01) 	logerror("CPU #0 - PC %04X: unknown nmi bits: %02X\n",activecpu_get_pc(),data);
+	switch(data)
+	{
+		case 0xa6: suna8_nmi_enable = 1; break;
+		case 0x00: suna8_nmi_enable = 0; break;
+		case 0x18: suna8_trash_prot = 0; break;
+		case 0x81: suna8_trash_prot = 1; break;
+		default: logerror("CPU #0 - PC %04X: unknown protection command: %02X\n",cpu_get_pc(space->cpu),data);
+	}
+}
+
+static WRITE_HANDLER( suna8_wram_w )
+{
+	if(!suna8_trash_prot)
+		suna8_wram[offset] = data;
 }
 
 /*
@@ -1370,11 +1403,11 @@ static MEMORY_WRITE_START( sparkman_writemem )
 	{ 0xc200, 0xc200, sparkman_spritebank_w		},	// Sprite RAM Bank
 	{ 0xc280, 0xc280, sparkman_rombank_w		},	// ROM Bank (?mirrored up to c2ff?)
 	{ 0xc300, 0xc300, sparkman_flipscreen_w		},	// Flip Screen
-	{ 0xc380, 0xc380, sparkman_nmi_w			},	// ? NMI related ?
+	{ 0xc380, 0xc3ff, sparkman_cmd_prot_w			},	// Protection
 	{ 0xc400, 0xc400, sparkman_leds_w			},	// Leds + Coin Counter
 	{ 0xc500, 0xc500, soundlatch_w				},	// To Sound CPU
 	{ 0xc600, 0xc7ff, paletteram_RRRRGGGGBBBBxxxx_swap_w, &paletteram	},	// Palette (Banked??)
-	{ 0xc800, 0xdfff, MWA_RAM					},	// RAM
+	{ 0xc800, 0xdfff, suna8_wram_w, &suna8_wram					},	// RAM
 	{ 0xe000, 0xffff, suna8_banked_spriteram_w	},	// Sprites (Banked)
 MEMORY_END
 
