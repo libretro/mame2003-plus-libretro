bcef83de6e94f5ac371ef900e19eec44d3ac21a3
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2021-07-19 00:23:34 -0400

    Update mame.h

--- src/mame.h
+++ src/mame.h
@@ -251,7 +251,7 @@ struct GameOptions
   bool     cheat_input_ports;     /*cheat input ports enable/disable */
   bool     machine_timing;
   bool     digital_joy_centering; /* center digital joysticks enable/disable */
-  double   over_clock;
+  double   cpu_clock_scale;
 #if (HAS_CYCLONE || HAS_DRZ80)
   int      cyclone_mode;
 #endif
