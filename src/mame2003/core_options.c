e754e5f023b5066857d8504a141b99077649e933
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2021-08-11 04:47:26 -0400

    Update core_options.c

--- src/mame2003/core_options.c
+++ src/mame2003/core_options.c
@@ -539,7 +539,7 @@ static struct retro_core_option_v2_definition option_def_nvram_bootstraps = {
    NULL,
    "Used to automatically initialize games that otherwise require special startup procedures or configurations.",
    NULL,
-   NULL,
+   "cat_key_system",
    {
       { "enabled",  NULL },
       { "disabled", NULL },
@@ -657,11 +657,11 @@ static struct retro_core_option_v2_definition option_def_core_save_subfolder = {
 
 static struct retro_core_option_v2_definition option_def_autosave_hiscore = {
    APPNAME"_autosave_hiscore",
-   "Autosave hiscore",
+   "Autosave Hiscore",
    NULL,
    "Recommended to use default which will save the hiscore when closing content. Recursively will save repeatedly the entire time during gameplay.",
    NULL,
-   NULL,
+   "cat_key_system",
    {
       { "default",     NULL },
       { "recursively", NULL },
