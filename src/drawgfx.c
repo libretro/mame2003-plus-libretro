a6d4b39de8e93e4d2d0eba47fed7402e014559e7
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2021-11-29 21:33:20 -0500

    Update drawgfx.c

--- src/drawgfx.c
+++ src/drawgfx.c
@@ -3527,6 +3527,7 @@ void draw_crosshair(struct mame_bitmap *bitmap,int x,int y,const struct rectangl
 	black = Machine->uifont->colortable[0];
 	white = Machine->uifont->colortable[1];
 
+	/* Crosshair */
 	for (i = 1;i < 6;i++)
 	{
 		plotclip(bitmap,x+i,y,white,clip);
@@ -3534,6 +3535,35 @@ void draw_crosshair(struct mame_bitmap *bitmap,int x,int y,const struct rectangl
 		plotclip(bitmap,x,y+i,white,clip);
 		plotclip(bitmap,x,y-i,white,clip);
 	}
+
+	/* straight bars */
+	for (i = -3;i < 4;i++)
+	{
+		plotclip(bitmap,x+6,y+i,white,clip);
+		plotclip(bitmap,x-6,y+i,white,clip);
+		plotclip(bitmap,x+i,y+6,white,clip);
+		plotclip(bitmap,x+i,y-6,white,clip);
+	}
+
+	/* 45 degrees */
+	plotclip(bitmap,x+4,y+5,white,clip);
+	plotclip(bitmap,x+5,y+4,white,clip);
+	plotclip(bitmap,x-4,y+5,white,clip);
+	plotclip(bitmap,x-5,y+4,white,clip);
+	plotclip(bitmap,x-4,y-5,white,clip);
+	plotclip(bitmap,x-5,y-4,white,clip);
+	plotclip(bitmap,x+4,y-5,white,clip);
+	plotclip(bitmap,x+4,y-4,white,clip);
+
+	/* Corner fill */
+	plotclip(bitmap,x+1,y+5,white,clip);
+	plotclip(bitmap,x+5,y+1,white,clip);
+	plotclip(bitmap,x-1,y+5,white,clip);
+	plotclip(bitmap,x-5,y+1,white,clip);
+	plotclip(bitmap,x-1,y-5,white,clip);
+	plotclip(bitmap,x-5,y-1,white,clip);
+	plotclip(bitmap,x+1,y-5,white,clip);
+	plotclip(bitmap,x+5,y-1,white,clip);
 }
 
 
