6dc6c911abf9fe45c08a1d9bd140a769a8d6efcd
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2021-12-04 15:41:48 -0500

    Update jaguar_vidhrdw.c

--- src/vidhrdw/jaguar_vidhrdw.c
+++ src/vidhrdw/jaguar_vidhrdw.c
@@ -761,11 +761,11 @@ VIDEO_UPDATE( cojag )
 
 		/* draw player 1's crosshair */
 		get_crosshair_xy(0, &beamx, &beamy);
-		draw_crosshair(bitmap, beamx, beamy, cliprect);
+		draw_crosshair(1, bitmap, beamx, beamy, cliprect);
 
 		/* draw player 2's crosshair */
 		get_crosshair_xy(1, &beamx, &beamy);
-		draw_crosshair(bitmap, beamx, beamy, cliprect);
+		draw_crosshair(2, bitmap, beamx, beamy, cliprect);
 	}
 }
 
