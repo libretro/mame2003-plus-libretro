eaf7f1c1c5ac88ed52232bbf6e8a8c55eb0a2363
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2021-12-07 11:27:50 -0500

    Create triplhnt.h

---
+++ src/includes/triplhnt.h
@@ -0,0 +1,17 @@
+/*************************************************************************
+
+	Atari Triple Hunt hardware
+
+*************************************************************************/
+
+/* Discrete Sound Input Nodes */
+#define TRIPLHNT_BEAR_ROAR_DATA	NODE_01
+#define TRIPLHNT_BEAR_EN		NODE_02
+#define TRIPLHNT_SHOT_DATA		NODE_03
+#define TRIPLHNT_SCREECH_EN		NODE_04
+#define TRIPLHNT_LAMP_EN		NODE_05
+
+
+/*----------- defined in sndhrdw/triplhnt.c -----------*/
+
+extern struct discrete_sound_block triplhnt_discrete_interface[];
