5b553f47d1ea6f98fb1b6cf977c075e9115065e7
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2021-12-07 16:58:21 -0500

    Update triplhnt.h

--- src/includes/triplhnt.h
+++ src/includes/triplhnt.h
@@ -16,3 +16,18 @@
 
 extern struct discrete_sound_block triplhnt_discrete_interface[];
 extern struct Samplesinterface triplhnt_samples_interface;
+
+
+/*----------- defined in vidhrdw/triplhnt.c -----------*/
+
+VIDEO_START( triplhnt );
+VIDEO_UPDATE( triplhnt );
+
+extern UINT8* triplhnt_playfield_ram;
+extern UINT8* triplhnt_vpos_ram;
+extern UINT8* triplhnt_hpos_ram;
+extern UINT8* triplhnt_code_ram;
+extern UINT8* triplhnt_orga_ram;
+
+extern int triplhnt_sprite_zoom;
+extern int triplhnt_sprite_bank;
