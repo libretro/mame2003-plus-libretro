ad19e3c9d1967d14aea1eb1636a11037af06e934
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2021-12-09 00:20:37 -0500

    Update system32.c

--- src/drivers/system32.c
+++ src/drivers/system32.c
@@ -1985,13 +1985,13 @@ INPUT_PORTS_START( jpark )
 	PORT_BIT( 0xff, IP_ACTIVE_LOW, IPT_UNKNOWN )
 
 	PORT_START	/* 0xc00050 - port 7  - player 1 analog X axis*/
-	PORT_ANALOG( 0xff, 0x99, IPT_LIGHTGUN_X | IPF_PLAYER1, 50, 5, 0x40, 0xc0 )
+	PORT_ANALOG( 0xff, 0x9c, IPT_LIGHTGUN_X | IPF_PLAYER1, 50, 5, 0x40, 0xc0 )
 
 	PORT_START	/* 0xc00052 - port 8  - player 1 analog Y axis*/
 	PORT_ANALOG( 0xff, 0x80, IPT_LIGHTGUN_Y | IPF_PLAYER1, 50, 5, 0x40, 0xc0 )
 
 	PORT_START	/* 0xc00054 - port 9  - player 2 analog X axis*/
-	PORT_ANALOG( 0xff, 0x66, IPT_LIGHTGUN_X | IPF_PLAYER2, 50, 5, 0x40, 0xc0 )
+	PORT_ANALOG( 0xff, 0x63, IPT_LIGHTGUN_X | IPF_PLAYER2, 50, 5, 0x40, 0xc0 )
 
 	PORT_START	/* 0xc00056 - port 10 - player 2 analog Y axis*/
 	PORT_ANALOG( 0xff, 0x80, IPT_LIGHTGUN_Y | IPF_PLAYER2, 50, 5, 0x40, 0xc0 )
