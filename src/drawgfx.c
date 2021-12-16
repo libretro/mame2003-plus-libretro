5b57794b6f899e0da1e5e3b92bea93c830610df4
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2021-12-16 06:18:52 -0500

    Update drawgfx.c

--- src/drawgfx.c
+++ src/drawgfx.c
@@ -3520,7 +3520,6 @@ void draw_crosshair(int player_number, struct mame_bitmap *bitmap,int x,int y,co
 {
 	unsigned long color,black,white;
 	int i;
-	float seconds = 15;
 	static int strobe = 0;
 	static bool polarity = true;
 	static int inactive_xy [MAX_PLAYER_COUNT][3];
@@ -3532,24 +3531,17 @@ void draw_crosshair(int player_number, struct mame_bitmap *bitmap,int x,int y,co
 		return;
 	}
 
-	/* Penalize seconds */
-	if(Machine->drv->frames_per_second > 60)
-		seconds += (Machine->drv->frames_per_second / 60);
-	else
-	seconds -= (Machine->drv->frames_per_second / 60);
-
 	/* Hide crosshairs for inactive players */
 	if(inactive_xy[player_number-1][0] == x && inactive_xy[player_number-1][1] == y)
 	{
-		if(inactive_xy[player_number-1][2] < (Machine->drv->frames_per_second * seconds))
-			inactive_xy[player_number-1][2]++;
-		else return;
+		if(inactive_xy[player_number-1][2] + (Machine->drv->frames_per_second * 15) < cpu_getcurrentframe())
+			return;
 	}
 	else
 	{
 		inactive_xy[player_number-1][0] = x;
 		inactive_xy[player_number-1][1] = y;
-		inactive_xy[player_number-1][2] = 0;
+		inactive_xy[player_number-1][2] = cpu_getcurrentframe();
 	}
 
 	/* Color crosshairs */
