6893b8e2816b3a036a1d950e6ddce337d4ba60e9
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2021-12-18 17:24:12 -0500

    Update williams.h

--- src/sndhrdw/williams.h
+++ src/sndhrdw/williams.h
@@ -20,15 +20,5 @@ void williams_narc_init(int cpunum);
 void williams_narc_data_w(int data);
 void williams_narc_reset_w(int state);
 
-extern bool	mk_playing_mortal_kombat; /* For Mortal Kombat music hack. For Midway Y-Unit versions.*/
-extern bool	mk_playing_mortal_kombat_t; /* For Mortal Kombat music hack. For Midway T-Unit versions.*/
-extern bool	nba_jam_playing; /* For NBA Jam music samples. For Midway T-Unit versions.*/
-extern bool	nba_jam_title_screen;
-extern bool	nba_jam_select_screen;
-extern bool	nba_jam_intermission;
-extern bool	nba_jam_in_game;
-extern bool	nba_jam_boot_up;
-extern bool	nba_jam_playing_title_music;
-
 extern int		m_nba_last_offset;
 extern int		m_nba_start_counter;
