555d92212da4ca4b2ebc8cc4fb63a75a3618f9d0
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2021-12-21 05:43:01 -0500

    Update ost_samples.h

--- src/ost_samples.h
+++ src/ost_samples.h
@@ -15,11 +15,15 @@ extern int      ddragon_stage;
 extern int      d_title_counter;
 
 extern bool     ff_playing_final_fight;
+extern bool     ff_provision_alt_song;
+extern bool     ff_play_alternate_song;
+
 extern bool     mk_playing_mortal_kombat;
 extern bool     mk_playing_mortal_kombat_t;
 
 extern bool     moonwalker_playing;
 extern bool     moon_diddy;
+extern int      mj_current_music;
 
 extern bool     nba_jam_playing;
 extern bool     nba_jam_title_screen;
