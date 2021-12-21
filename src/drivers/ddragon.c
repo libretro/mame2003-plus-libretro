d84cd84c331b27711e59fac880568f3febc7037c
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2021-12-20 23:18:18 -0500

    Update ddragon.c

--- src/drivers/ddragon.c
+++ src/drivers/ddragon.c
@@ -280,6 +280,10 @@ static WRITE_HANDLER( ddragon_interrupt_w )
 	case 3: /* 380e - SND irq */
 		if(ddragon_playing && options.use_samples) {
 			generate_ost_sound_ddragon( data );
+			if(schedule_default_sound) {
+				soundlatch_w( 0, data );
+				cpu_set_irq_line( snd_cpu, sound_irq, (sound_irq == IRQ_LINE_NMI) ? PULSE_LINE : HOLD_LINE );
+			}
 		}
 		else {
 			soundlatch_w( 0, data );
