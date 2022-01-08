35a275d8125da4e319a6439174f36d37189bc2d3
arcadez2003 <51386955+arcadez2003@users.noreply.github.com>
2022-01-08 21:51:21 +0000

    snowbros 3 video call

--- src/vidhrdw/snowbros_vidhrdw.c
+++ src/vidhrdw/snowbros_vidhrdw.c
@@ -135,3 +135,90 @@ VIDEO_UPDATE( wintbob )
 		}
 	}
 }
+
+
+VIDEO_UPDATE( snowbro3 )
+{
+
+	int sx=0, sy=0, x=0, y=0, offs;
+
+	/*
+	 * Sprite Tile Format
+	 * ------------------
+	 *
+	 * Byte | Bit(s)   | Use
+	 * -----+-76543210-+----------------
+	 *	0-5	| -------- | ?
+	 *	6	| -------- | ?
+	 *	7	| xxxx.... | Palette Bank
+	 *	7	| .......x | XPos - Sign Bit
+	 *	9	| xxxxxxxx | XPos
+	 *	7	| ......x. | YPos - Sign Bit
+	 *	B	| xxxxxxxx | YPos
+	 *	7	| .....x.. | Use Relative offsets
+	 *	C	| -------- | ?
+	 *	D	| xxxxxxxx | Sprite Number (low 8 bits)
+	 *	E	| -------- | ?
+	 *	F	| ....xxxx | Sprite Number (high 4 bits)
+	 *	F	| x....... | Flip Sprite Y-Axis
+	 *	F	| .x...... | Flip Sprite X-Axis
+	 */
+
+	/* This clears & redraws the entire screen each pass */
+
+	fillbitmap(bitmap,get_black_pen(),&Machine->visible_area);
+
+	for (offs = 0;offs < spriteram_size/2;offs += 8)
+	{
+		struct GfxElement *gfx = Machine->gfx[0];
+		int dx = spriteram16[offs+4] & 0xff;
+		int dy = spriteram16[offs+5] & 0xff;
+		int tilecolour = spriteram16[offs+3];
+		int attr = spriteram16[offs+7];
+		int flipx =   attr & 0x80;
+		int flipy =  (attr & 0x40) << 1;
+		int tile  = ((attr & 0xff) << 8) + (spriteram16[offs+6] & 0xff);
+
+		if (tilecolour & 1) dx = -1 - (dx ^ 0xff);
+		if (tilecolour & 2) dy = -1 - (dy ^ 0xff);
+		if (tilecolour & 4)
+		{
+			x += dx;
+			y += dy;
+		}
+		else
+		{
+			x = dx;
+			y = dy;
+		}
+
+		if (x > 511) x &= 0x1ff;
+		if (y > 511) y &= 0x1ff;
+
+		if (flip_screen)
+		{
+			sx = 240 - x;
+			sy = 240 - y;
+			flipx = !flipx;
+			flipy = !flipy;
+		}
+		else
+		{
+			sx = x;
+			sy = y;
+		}
+
+		if (offs < 0x800) /* i guess this is the right way */
+		{
+			gfx = Machine->gfx[1];
+			tilecolour = 0x10;
+		}
+
+	drawgfx(bitmap,gfx,
+				tile,
+				(tilecolour & 0xf0) >> 4,
+				flipx, flipy,
+				sx,sy,
+				&Machine->visible_area,TRANSPARENCY_PEN,0);
+	}
+}
