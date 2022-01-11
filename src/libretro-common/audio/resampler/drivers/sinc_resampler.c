35c16607e8cfa44fe8afb685460c7f86d03ef215
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2022-01-10 19:32:50 -0500

    resync

--- src/libretro-common/audio/resampler/drivers/sinc_resampler.c
+++ src/libretro-common/audio/resampler/drivers/sinc_resampler.c
@@ -85,31 +85,25 @@ typedef struct rarch_sinc_resampler
    float kaiser_beta;
 } rarch_sinc_resampler_t;
 
-#if (defined(__ARM_NEON__) && !defined(DONT_WANT_ARM_OPTIMIZATIONS)) || defined(HAVE_NEON)
-#if TARGET_OS_IPHONE
-#else
-#ifndef WANT_NEON
-#define WANT_NEON
-#endif
-#endif
-#endif
+#if (defined(__ARM_NEON__) || defined(HAVE_NEON))
 
-#ifdef WANT_NEON
-/* Assumes that taps >= 8, and that taps is a multiple of 8. */
+#ifdef HAVE_ARM_NEON_ASM_OPTIMIZATIONS
 void process_sinc_neon_asm(float *out, const float *left,
       const float *right, const float *coeff, unsigned taps);
+#else
+#include <arm_neon.h>
+#endif
 
+/* Assumes that taps >= 8, and that taps is a multiple of 8. */
 static void resampler_sinc_process_neon(void *re_, struct resampler_data *data)
 {
    rarch_sinc_resampler_t *resamp = (rarch_sinc_resampler_t*)re_;
    unsigned phases                = 1 << (resamp->phase_bits + resamp->subphase_bits);
-
    uint32_t ratio                 = phases / data->ratio;
    const float *input             = data->data_in;
    float *output                  = data->data_out;
    size_t frames                  = data->input_frames;
    size_t out_frames              = 0;
-
    while (frames)
    {
       while (frames && resamp->time >= phases)
@@ -120,10 +114,10 @@ static void resampler_sinc_process_neon(void *re_, struct resampler_data *data)
          resamp->ptr--;
 
          resamp->buffer_l[resamp->ptr + resamp->taps] =
-         resamp->buffer_l[resamp->ptr]                = *input++;
+            resamp->buffer_l[resamp->ptr]                = *input++;
 
          resamp->buffer_r[resamp->ptr + resamp->taps] =
-         resamp->buffer_r[resamp->ptr]                = *input++;
+            resamp->buffer_r[resamp->ptr]                = *input++;
 
          resamp->time                                -= phases;
          frames--;
@@ -137,12 +131,32 @@ static void resampler_sinc_process_neon(void *re_, struct resampler_data *data)
          {
             unsigned phase           = resamp->time >> resamp->subphase_bits;
             const float *phase_table = resamp->phase_table + phase * taps;
-
+#ifdef HAVE_ARM_NEON_ASM_OPTIMIZATIONS
             process_sinc_neon_asm(output, buffer_l, buffer_r, phase_table, taps);
+#else
+            unsigned i;
+            float32x4_t p1 = {0, 0, 0, 0}, p2 = {0, 0, 0, 0};
+            float32x2_t p3, p4;
 
-            output += 2;
+            for (i = 0; i < taps; i += 8)
+            {
+               float32x4x2_t coeff8  = vld2q_f32(&phase_table[i]);
+               float32x4x2_t left8   = vld2q_f32(&buffer_l[i]);
+               float32x4x2_t right8  = vld2q_f32(&buffer_r[i]);
+
+               p1 = vmlaq_f32(p1,  left8.val[0], coeff8.val[0]);
+               p2 = vmlaq_f32(p2, right8.val[0], coeff8.val[0]);
+               p1 = vmlaq_f32(p1,  left8.val[1], coeff8.val[1]);
+               p2 = vmlaq_f32(p2, right8.val[1], coeff8.val[1]);
+            }
+
+            p3 = vadd_f32(vget_low_f32(p1), vget_high_f32(p1));
+            p4 = vadd_f32(vget_low_f32(p2), vget_high_f32(p2));
+            vst1_f32(output, vpadd_f32(p3, p4));
+#endif
+            output                 += 2;
             out_frames++;
-            resamp->time += ratio;
+            resamp->time           += ratio;
          }
       }
    }
@@ -844,7 +858,7 @@ static void *resampler_sinc_new(const struct resampler_config *config,
    else
 #endif
    {
-#if defined(WANT_NEON)
+#if (defined(__ARM_NEON__) || defined(HAVE_NEON))
       re->taps     = (re->taps + 7) & ~7;
 #else
       re->taps     = (re->taps + 3) & ~3;
@@ -902,7 +916,7 @@ static void *resampler_sinc_new(const struct resampler_config *config,
    }
    else if (mask & RESAMPLER_SIMD_NEON && window_type != SINC_WINDOW_KAISER)
    {
-#if defined(WANT_NEON)
+#if (defined(__ARM_NEON__) || defined(HAVE_NEON))
       sinc_resampler.process = resampler_sinc_process_neon;
 #endif
    }
@@ -922,5 +936,3 @@ retro_resampler_t sinc_resampler = {
    "sinc",
    "sinc"
 };
-
-#undef WANT_NEON
