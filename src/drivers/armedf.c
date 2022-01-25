fdd407b4d2a6faa60a551b5a233f02a714018c89
arcadez2003 <51386955+arcadez2003@users.noreply.github.com>
2022-01-25 23:01:55 +0000

    Fix Chouji Meikyuu Legion
    MAME WIP
    Tomasz Slanina Fixed text layer priority in Chouji Meikyuu Legion (both sets).

--- src/drivers/armedf.c
+++ src/drivers/armedf.c
@@ -1046,7 +1046,7 @@ static MACHINE_DRIVER_START( legiono )
 	MDRV_PALETTE_LENGTH(2048)
 
 	MDRV_VIDEO_EOF(armedf)
-	MDRV_VIDEO_START(armedf)
+	MDRV_VIDEO_START(legion)
 	MDRV_VIDEO_UPDATE(armedf)
 
 	/* sound hardware */
@@ -1334,8 +1334,8 @@ DRIVER_INIT( cclimbr2 )
 
 
 /*     YEAR, NAME,   PARENT,   MACHINE,  INPUT,    INIT,     MONITOR, COMPANY,     FULLNAME, FLAGS */
-GAMEX( 1987, legion,   0,      legion,   legion,   legion,   ROT270, "Nichibutsu", "Legion (ver 2.03)",  GAME_IMPERFECT_GRAPHICS | GAME_UNEMULATED_PROTECTION )
-GAMEX( 1987, legiono,  legion, legiono,  legion,   legiono,  ROT270, "Nichibutsu", "Legion (ver 1.05)",  GAME_IMPERFECT_GRAPHICS | GAME_UNEMULATED_PROTECTION | GAME_NOT_WORKING )
+GAMEX( 1987, legion,   0,      legion,   legion,   legion,   ROT270, "Nichibutsu", "Legion - Spinner-87 (World ver 2.03)",  GAME_IMPERFECT_GRAPHICS | GAME_UNEMULATED_PROTECTION )
+GAMEX( 1987, legiono,  legion, legiono,  legion,   legiono,  ROT270, "Nichibutsu", "'Chouji Meikyuu Legion (Japan bootleg ver 1.05)",  GAME_IMPERFECT_GRAPHICS ) /* blitter protection removed */
 GAMEX( 1987, terraf,   0,      terraf,   terraf,   terraf,   ROT0,   "Nichibutsu", "Terra Force",  GAME_IMPERFECT_GRAPHICS | GAME_UNEMULATED_PROTECTION )
 GAMEX( 1987, terrafu,  terraf, terraf,   terraf,   terrafu,  ROT0,   "Nichibutsu USA", "Terra Force (US)",  GAME_IMPERFECT_GRAPHICS | GAME_UNEMULATED_PROTECTION )
 GAMEX( 1987, kodure,   0,      kodure,   kodure,   kodure,   ROT0,   "Nichibutsu", "Kodure Ookami (Japan)",  GAME_IMPERFECT_GRAPHICS | GAME_UNEMULATED_PROTECTION )
