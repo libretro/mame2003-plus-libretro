d337c7541f447f2c669b850addae0e1c7473030b
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2022-05-27 03:04:24 -0400

    Update batman.c

--- src/drivers/batman.c
+++ src/drivers/batman.c
@@ -353,7 +353,7 @@ static struct GfxLayout molayout =
 
 static struct GfxDecodeInfo gfxdecodeinfo2[] =
 {
-	{  REGION_GFX1, 0, &pflayout,   256, 1  },
+	{  REGION_GFX1, 0, &pflayout,   0, 1  },
 	{  REGION_GFX2, 0, &molayout,   0, 0x10  },
 	{ -1 }
 };
