e2a0c00834d8a2e62ab43e043d2f04510e9c9686
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2022-05-30 11:58:32 -0400

    Update system16.c

--- src/drivers/system16.c
+++ src/drivers/system16.c
@@ -6776,30 +6776,30 @@ MEMORY_END
 
 static MEMORY_READ16_START( fantzn2x_readmem )
 	{ 0x000000, 0x0bffff, MRA16_ROM },
-	{ 0x200000, 0x23FFFF, SYS16_MRA16_WORKINGRAM }, // correct for extra ram board.??
-	{ 0x3F0000, 0x3FFFFF, SYS16_MRA16_EXTRAM, }, // rom_5704_bank
-	{ 0x400000, 0x40FFFF, SYS16_MRA16_TILERAM },
-	{ 0x410000, 0x410FFF, SYS16_MRA16_TEXTRAM },
-	{ 0x440000, 0x440FFF, SYS16_MRA16_SPRITERAM },
-	{ 0x840000, 0x840FFF, SYS16_MRA16_PALETTERAM },
+	{ 0x200000, 0x23ffff, SYS16_MRA16_WORKINGRAM }, // correct for extra ram board.??
+	{ 0x3F0000, 0x3fffff, SYS16_MRA16_EXTRAM, }, // rom_5704_bank
+	{ 0x400000, 0x40ffff, SYS16_MRA16_TILERAM },
+	{ 0x410000, 0x410fff, SYS16_MRA16_TEXTRAM },
+	{ 0x440000, 0x440fff, SYS16_MRA16_SPRITERAM },
+	{ 0x840000, 0x840fff, SYS16_MRA16_PALETTERAM },
 	{ 0xc41000, 0xc41001, input_port_2_word_r }, // service
-	{ 0XC41002, 0xc41003, input_port_0_word_r }, // player1
+	{ 0Xc41002, 0xc41003, input_port_0_word_r }, // player1
 	{ 0xc41006, 0xc41007, input_port_1_word_r }, // player2
 	{ 0xc42000, 0xc42001, input_port_4_word_r }, // dsw1
 	{ 0xc42002, 0xc42003, input_port_3_word_r }, // coinage
-	{ 0xFE0006, 0xFE0006, ww_io_service_r },
+	{ 0xfe0006, 0xfe0007, ww_io_service_r },
 MEMORY_END
 
 static MEMORY_WRITE16_START( fantzn2x_writemem )
 	{ 0x000000, 0x0bffff, MWA16_ROM },
-	{ 0x200000, 0x23FFFF, SYS16_MWA16_WORKINGRAM },
-	{ 0x3F0000, 0x3FFFFF, SYS16_MWA16_EXTRAM, &sys16_extraram}, //rom_5704_bank
-	{ 0x400000, 0x40FFFF, SYS16_MWA16_TILERAM, &sys16_tileram },
-	{ 0x410000, 0x410FFF, SYS16_MWA16_TEXTRAM, &sys16_textram },
-	{ 0x440000, 0x440FFF, SYS16_MWA16_SPRITERAM, &sys16_spriteram },
-	{ 0x840000, 0x840FFF, SYS16_MWA16_PALETTERAM, &paletteram16 },
+	{ 0x200000, 0x23ffff, SYS16_MWA16_WORKINGRAM },
+	{ 0x3F0000, 0x3fffff, SYS16_MWA16_EXTRAM, &sys16_extraram}, //rom_5704_bank
+	{ 0x400000, 0x40ffff, SYS16_MWA16_TILERAM, &sys16_tileram },
+	{ 0x410000, 0x410fff, SYS16_MWA16_TEXTRAM, &sys16_textram },
+	{ 0x440000, 0x440fff, SYS16_MWA16_SPRITERAM, &sys16_spriteram },
+	{ 0x840000, 0x840fff, SYS16_MWA16_PALETTERAM, &paletteram16 },
 	{ 0xc40000, 0xc40001, sys16_coinctrl_w },
-	{ 0xFE0006, 0xFE0006, sound_command_w }, // this was best guess need verfied
+	{ 0xfe0006, 0xfe0007, sound_command_w }, // this was best guess need verfied
 MEMORY_END
 
 /***************************************************************************/
