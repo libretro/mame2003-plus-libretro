d90e64607bbb11ba42841c94e81478d9236b35ad
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2022-06-10 04:10:43 -0400

    Update system16.c

--- src/drivers/system16.c
+++ src/drivers/system16.c
@@ -5996,7 +5996,7 @@ static MACHINE_INIT( tturfbl ){
 	};
 	sys16_obj_bank = bank;
 	sys16_sprite_draw = 1;
-	sys16_sprxoffset = 0x200;
+	sys16_sprxoffset = -0x48;
 
 	sys16_update_proc = tturfbl_update_proc;
 }
