1ae074bed30aa8008eba4c301fa12b0f83b825d3
MistyDreams <MistyDreams@dreams.com>
2022-06-15 09:34:54 +0100

    fix cotton sound without core update

--- src/machine/system16_machine.c
+++ src/machine/system16_machine.c
@@ -2145,7 +2145,8 @@ void aurail_decode_opcode2(data16_t *dest,data16_t *source,int size)
 
 static void sound_cause_nmi( int chip ){
 	/* upd7759 callback */
-	cpu_set_nmi_line(1, PULSE_LINE);
+	if (chip)
+		cpu_set_nmi_line(1, PULSE_LINE);
 }
 
 struct SEGAPCMinterface sys16_segapcm_interface_15k = {
