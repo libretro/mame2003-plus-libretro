a48913c71ef5c28102441e53b00cb48b319af4a1
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2022-06-24 18:04:31 -0400

    Update system16.c

--- src/drivers/system16.c
+++ src/drivers/system16.c
@@ -2054,7 +2054,7 @@ static MACHINE_INIT( bodyslam ){
 	sys16_sprxoffset = -0xbd;
 	sys16_fgxoffset = sys16_bgxoffset = 7;
 	sys16_bg_priority_mode = 2;
-	sys16_fg_priority_mode = 2;
+	sys16_bg_priority_value = 0x0a00;
 
 	sys16_textlayer_lo_min=0;
 	sys16_textlayer_lo_max=0x1f;
