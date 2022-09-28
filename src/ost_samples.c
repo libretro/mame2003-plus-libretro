e1c99e7e45951fd96fa7c9d7656f91972ddeeac0
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2022-09-28 17:02:34 -0400

    Update ost_samples.c

--- src/ost_samples.c
+++ src/ost_samples.c
@@ -1778,7 +1778,7 @@ bool generate_ost_sound_nba_jam(int data)
 			break;
 	}
 
-	if(data!=2 && data!=170) usrintf_showmessage("data:%i  last:%i %i", data, last_left, last_right);
+	log_cb(RETRO_LOG_INFO, LOGPRE "data:%i  last:%i %i/n", data, last_left, last_right);
 
 	ost_mix_samples();
 
