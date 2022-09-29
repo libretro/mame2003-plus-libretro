a66c779beb1a1c36832f4dea8660927b647d5692
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2022-09-28 20:10:47 -0400

    Update ost_samples.c

--- src/ost_samples.c
+++ src/ost_samples.c
@@ -1711,7 +1711,7 @@ bool generate_ost_sound_nba_jam(int data)
 
 		/* 1st quarter.*/
 		case 0x2:
-			schedule_default_sound = true;
+			/* Bug - announcer repeats opponent's name with this trigger. This default sound needs returned but then it plays over our sample.*/
 			ost_start_samples(4, 5, 1);
 			break;
 
