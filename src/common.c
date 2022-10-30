d3191915922b117e4bf8be9bab0a0a9a907a2e66
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2022-10-30 09:29:30 -0400

    Update common.c

--- src/common.c
+++ src/common.c
@@ -1731,7 +1731,7 @@ static int process_rom_entries(struct rom_load_data *romdata, const struct RomMo
 				if (!open_rom_file(romdata, romp))
 				{
 					if (ROM_GETBIOSFLAGS(romp) == (system_bios+1))
-						{ log_cb(RETRO_LOG_INFO,"default bios fallback.\n"); romp=NULL; system_bios=0; continue;} /*fallback to default bios.*/
+						{ log_cb(RETRO_LOG_WARN, LOGPRE "default bios fallback.\n"); system_bios=0; continue;} /*fallback to default bios.*/
 			
 					else
 						handle_missing_file(romdata, romp);
