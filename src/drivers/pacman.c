7dcf385f3ba20be38bffc96b02de3db829ed7b58
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2022-11-16 07:52:26 -0500

    Update pacman.c

--- src/drivers/pacman.c
+++ src/drivers/pacman.c
@@ -4215,7 +4215,7 @@ ROM_START( nmouseb )
 	ROM_LOAD( "82s126.3m", 0x0100, 0x0100, CRC(77245b66) SHA1(0c4d0bee858b97632411c440bea6948a74759746) )   /*timing - not used */
 ROM_END
 
-ROM_START( woodpek )
+ROM_START( woodpeck )
 	ROM_REGION( 0x10000, REGION_CPU1,0 )     /* 64k for code */
 	/* roms dumped from epoxy block */
 	ROM_LOAD( "f.bin", 0x0000, 0x1000, CRC(37ea66ca) SHA1(1779e2af8ffc72ec454a401cf6fa93e77e28576a) )
@@ -4224,13 +4224,11 @@ ROM_START( woodpek )
 	ROM_LOAD( "g.bin", 0xa000, 0x1000, CRC(024092f4) SHA1(4b16a3ff101397af64fc89d9f93bbdb939c8e699) )
 	ROM_LOAD( "h.bin", 0xb000, 0x1000, CRC(18ef0fc8) SHA1(4cf3854adbcdd4ca2d855c48acff39fce5be48f7) )
 
-	ROM_REGION( 0x1000, REGION_GFX1 , ROMREGION_DISPOSE)
+	ROM_REGION( 0x1000, REGION_GFX1 , 0)
 	ROM_LOAD( "a.5e",  0x0000, 0x0800, CRC(15a87f62) SHA1(df6a6594ea8c6957e420b95e25ca33a9add13c09) )
 	ROM_LOAD( "c.5h",  0x0800, 0x0800, CRC(ab4abd88) SHA1(04199a127556159878d719599d57a3548eb14a3c) )	
-
-	ROM_REGION( 0x1000, REGION_GFX2 , ROMREGION_DISPOSE )
-	ROM_LOAD( "b.5f",  0x0000, 0x0800, CRC(5b9ba95b) SHA1(6d963da936c26830a614b69c663fc1e20b70f9dc) )
-	ROM_LOAD( "d.5j",  0x0800, 0x0800, CRC(d7b80a45) SHA1(8f4ef319b960ae0e2cb30910b7efe6c0691df2bb) )
+	ROM_LOAD( "b.5f",  0x1000, 0x0800, CRC(5b9ba95b) SHA1(6d963da936c26830a614b69c663fc1e20b70f9dc) )
+	ROM_LOAD( "d.5j",  0x1800, 0x0800, CRC(d7b80a45) SHA1(8f4ef319b960ae0e2cb30910b7efe6c0691df2bb) )
 
 	ROM_REGION( 0x0120, REGION_PROMS, 0 )
 	ROM_LOAD( "pr.8h", 0x0000, 0x0020, CRC(2fc650bd) SHA1(8d0268dee78e47c712202b0ec4f1f51109b1f2a5) )
@@ -4540,9 +4538,6 @@ static DRIVER_INIT( woodpek )
 	/* Data lines D4 and D6 and address lines A0 and A2 are swapped */
 	RAM = memory_region(REGION_GFX1);
 	for (i = 0;i < memory_region_length(REGION_GFX1);i += 8)
-		eyes_decode(&RAM[i]);
-	RAM = memory_region(REGION_GFX2);
-	for (i = 0;i < memory_region_length(REGION_GFX2);i += 8)
 		eyes_decode(&RAM[i]);
 }
 
