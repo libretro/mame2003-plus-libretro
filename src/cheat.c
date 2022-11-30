d2e633f6d43ec221c19784ac3ed0db9d84f09e1a
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2022-11-29 23:36:20 -0500

    Update cheat.c

--- src/cheat.c
+++ src/cheat.c
@@ -385,7 +385,7 @@ is selected
 
 #define OSD_READKEY_KLUDGE	1
 
-#define DESC_MAX_LENGTH		256
+#define DESC_MAX_LENGTH		255
 #define STR_(X) #X
 #define STR(X) STR_(X)
 
@@ -8316,7 +8316,7 @@ static void LoadCheatDatabase()
 		int			data;
 		int			extendData;
 		char		name[256];
-		char		description[DESC_MAX_LENGTH];
+		char		description[DESC_MAX_LENGTH+1];
 
 		int			argumentsMatched;
 
