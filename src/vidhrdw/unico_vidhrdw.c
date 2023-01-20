0f4e069e97490bce01ea333f84096a72af185a6e
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2023-01-20 17:06:29 -0500

    Update unico_vidhrdw.c

--- src/vidhrdw/unico_vidhrdw.c
+++ src/vidhrdw/unico_vidhrdw.c
@@ -381,8 +381,9 @@ if ( keyboard_pressed(KEYCODE_Z) || keyboard_pressed(KEYCODE_X) )
 	/* Sprites are drawn last, using pdrawgfx */
 	if (layers_ctrl & 8)	unico_draw_sprites(bitmap,cliprect);
 
-	/* Draw the gunsight for ligth gun games */
+	/* Draw the gunsight for light gun games */
 	if (unico_has_lightgun) {
+		usrintf_showmessage("crosshair");
 		draw_crosshair(1, bitmap,
 			readinputport(6)*384/256,
 			readinputport(5)*224/256,
