034815e6e18ea34bf5d919b11c82e2497b847977
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2023-01-21 09:21:32 -0500

    Update deco32_vidhrdw.c

--- src/vidhrdw/deco32_vidhrdw.c
+++ src/vidhrdw/deco32_vidhrdw.c
@@ -1496,8 +1496,8 @@ VIDEO_UPDATE( dragngun )
 
 	if (lockload_crosshairs)
 	{
-		draw_crosshair(1, bitmap,readinputport(6)*1.25,readinputport(7),cliprect);
-		draw_crosshair(2, bitmap,readinputport(8)*320/256,readinputport(9),cliprect);
+		draw_crosshair(1, bitmap,readinputport(6)*320/256,readinputport(7)*248/256,cliprect);
+		draw_crosshair(2, bitmap,readinputport(8)*320/256,readinputport(9)*248/256,cliprect);
 	}
 }
 
