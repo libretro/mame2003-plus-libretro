8337a2ad735c12dfe74bd40ca0c4164a1e737637
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2023-02-10 07:44:05 -0500

    Allow paddle too

--- src/inptport.c
+++ src/inptport.c
@@ -897,12 +897,14 @@ void update_analog_port(int port)
 
 	player = IP_GET_PLAYER(in);
 
-    /* if second player on a dial, and dial sharing turned on, use Y axis from player 1 */
-    if (options.dial_share_xy && type == IPT_DIAL && player == 1)
-    {
-        axis = Y_AXIS;
-        player = 0;
-    }
+	/* if second player on a dial or paddle, and dial sharing turned on, use Y axis from player 1 */
+	if ( (type == IPT_DIAL || type == IPT_PADDLE) && player == 1) {
+		if (options.dial_share_xy)
+		{
+			axis = Y_AXIS;
+			player = 0;
+		}
+	}
 
 	delta = mouse_delta_axis[player][axis];
 
