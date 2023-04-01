b7a298ee43a10cbed02baab1812050c31dfe98e1
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2023-04-01 15:24:41 -0400

    Update williams_sndhrdw.c

--- src/sndhrdw/williams_sndhrdw.c
+++ src/sndhrdw/williams_sndhrdw.c
@@ -600,10 +600,10 @@ static READ_HANDLER( adpcm_command_r )
 void williams_adpcm_data_w(int data)
 {
 	if( ost_support_enabled(OST_SUPPORT_NBA_JAM) ) {
-		if(generate_ost_sound_nba_jam( data )) soundlatch_w(0, data & 0xff);
+		if(generate_ost_sound( data )) soundlatch_w(0, data & 0xff);
 	}
 	else if( ost_support_enabled(OST_SUPPORT_MK) ) {
-		if(generate_ost_sound_mk( data )) soundlatch_w(0, data & 0xff);
+		if(generate_ost_sound( data )) soundlatch_w(0, data & 0xff);
 	}
 	else {
 		soundlatch_w(0, data & 0xff);
