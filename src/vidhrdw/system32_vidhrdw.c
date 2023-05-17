d44188c149e6412dbeff6f7f00a407827313479f
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2023-05-17 17:47:13 -0400

    Update system32_vidhrdw.c

--- src/vidhrdw/system32_vidhrdw.c
+++ src/vidhrdw/system32_vidhrdw.c
@@ -1558,7 +1558,10 @@ VIDEO_UPDATE( system32 ) {
 				if (!(sys32_tmap_disabled & 0x2)) system32_draw_bg_layer (bitmap,cliprect,1);
 			}
 			if (priloop == priority2 && (!multi32 || (multi32 && (readinputport(0xf)&1)))) {
-				if (!(sys32_tmap_disabled & 0x4)) system32_draw_bg_layer (bitmap,cliprect,2);
+				if (!(sys32_tmap_disabled & 0x4)) {
+          if (priloop==0xe) system32_draw_bg_layer (bitmap,cliprect,1);
+          system32_draw_bg_layer (bitmap,cliprect,2);
+        }
 			}
 			if (priloop == priority3 && (!multi32 || (multi32 && (readinputport(0xf)&2)>>1))) {
 				if (!(sys32_tmap_disabled & 0x8)) system32_draw_bg_layer (bitmap,cliprect,3);
