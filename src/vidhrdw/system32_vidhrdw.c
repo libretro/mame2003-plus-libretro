b1e214d75777154bdb122282bd9e52ca1d4e83eb
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2023-05-19 09:38:08 -0400

    Update system32_vidhrdw.c

--- src/vidhrdw/system32_vidhrdw.c
+++ src/vidhrdw/system32_vidhrdw.c
@@ -821,7 +821,7 @@ static INLINE void system32_get_sprite_info ( struct mame_bitmap *bitmap, const 
 
 	/* Inefficient sprite priority hack to get things working for now.  Will change to arrays later.
 		Currently, draw_sprite is a lot more processor intensive and has a greater need for optimisation. */
-	if (priloop==sys32sprite_priority & 0x8)
+	if (priloop==(sys32sprite_priority*2)&0xf )
 		if (!multi32 || (multi32 && (readinputport(0xf)&(sys32sprite_monitor_select+1))>>sys32sprite_monitor_select))
 			system32_draw_sprite ( bitmap, cliprect );
 }
