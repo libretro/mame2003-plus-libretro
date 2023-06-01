2737329a3aabf14b4ec7e594f6ae5c0a8770ac66
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2023-06-01 10:16:01 -0400

    Update system32_vidhrdw.c

--- src/vidhrdw/system32_vidhrdw.c
+++ src/vidhrdw/system32_vidhrdw.c
@@ -333,7 +333,7 @@ static INLINE void system32_draw_sprite ( struct mame_bitmap *bitmap, const stru
 			do {
 				do {
 					if (src_ptr[edx]) eax = src_ptr[edx];
-					else continue;
+					else eax=1;
 					edx = src_fx;
 					if (src_fx & FPONE) eax &= 0xf; else eax >>= 4;
 					edx += src_fdx;
