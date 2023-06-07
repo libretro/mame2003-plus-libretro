f71f0abcb528e6b3cb90a3d550224015b5f84ba8
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2023-06-06 20:55:35 -0400

    Fix sonic attract screen graphics (#1601)

--- src/vidhrdw/system32_vidhrdw.c
+++ src/vidhrdw/system32_vidhrdw.c
@@ -1375,7 +1375,8 @@ void system32_draw_bg_layer ( struct mame_bitmap *bitmap, const struct rectangle
 		tableaddress = (tableaddress * 0x200);
 
 		if ((system32_mixerregs[monitor][(0x32+layer*2)/2]&8)>>3) {
-			if (layer==2) tilemap_set_flip(system32_layer_tilemap[layer], TILEMAP_FLIPX);
+			/* sonic trips this during attract mode on the ice level demo, messes up layer 2 bg. mixer register maybe wrong? */
+			if (layer==2 && strcmp(Machine->gamedrv->name,"sonic")) tilemap_set_flip(system32_layer_tilemap[layer], TILEMAP_FLIPX);
 		}
 
 		for (line = 0; line < 224;line++) {
