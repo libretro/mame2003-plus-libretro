1af4b556f784803ed11b613a91a3fefd25068413
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2023-06-08 09:44:50 -0400

    Fix alien3 enter initials high score screen. (#1603)

--- src/vidhrdw/system32_vidhrdw.c
+++ src/vidhrdw/system32_vidhrdw.c
@@ -1374,9 +1374,14 @@ void system32_draw_bg_layer ( struct mame_bitmap *bitmap, const struct rectangle
 
 		tableaddress = (tableaddress * 0x200);
 
-		if ((system32_mixerregs[monitor][(0x32+layer*2)/2]&8)>>3) {
-			/* sonic trips this during attract mode on the ice level demo, messes up layer 2 bg. mixer register maybe wrong? */
-			if (layer==2 && strcmp(Machine->gamedrv->name,"sonic")) tilemap_set_flip(system32_layer_tilemap[layer], TILEMAP_FLIPX);
+		/* workaround for issue with system32_mixerregs. */
+		/* sonic  - trips this during attract mode on the ice level demo, messes up layer 2 bg. */
+		/* alien3 - trips this during the enter initials high score screen, flips the letters. */
+		if ( strcmp(Machine->gamedrv->name,"sonic") && strcmp(Machine->gamedrv->name,"alien3") )
+		{
+			if ((system32_mixerregs[monitor][(0x32+layer*2)/2]&8)>>3) {
+				if (layer==2) tilemap_set_flip(system32_layer_tilemap[layer], TILEMAP_FLIPX);
+			}
 		}
 
 		for (line = 0; line < 224;line++) {
