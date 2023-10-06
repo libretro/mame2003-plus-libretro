62e3241d2523287852c5e7a3b016368592b2e70a
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2023-10-06 08:08:29 -0400

    Update system32_vidhrdw.c

--- src/vidhrdw/system32_vidhrdw.c
+++ src/vidhrdw/system32_vidhrdw.c
@@ -1434,16 +1434,16 @@ void system32_draw_bg_layer_zoom ( struct mame_bitmap *bitmap, const struct rect
 	/* Draw */
 	tilemap_set_scrollx(system32_layer_tilemap[layer],0,((sys32_videoram[(0x01FF12+8*layer)/2]) & 0x3ff));
 	tilemap_set_scrolly(system32_layer_tilemap[layer],0,((sys32_videoram[(0x01FF16+8*layer)/2]) & 0x1ff));
-	tilemap_set_scrolldx(system32_layer_tilemap[layer], (sys32_videoram[0x1ff30/2 + 2 * layer]&0x1ff)+monitor*monitor_res, -(sys32_videoram[0x1ff30/2 + 2 * layer]&0x1ff)-monitor*monitor_res);
-	tilemap_set_scrolldy(system32_layer_tilemap[layer], sys32_videoram[0x1ff32/2 + 2 * layer]&0x1ff, -sys32_videoram[0x1ff32/2 + 2 * layer]&0x1ff);
+	tilemap_set_scrolldx(system32_layer_tilemap[layer], (sys32_videoram[0x1ff30/2 + 2 * layer])+monitor*monitor_res, -(sys32_videoram[0x1ff30/2 + 2 * layer])-monitor*monitor_res);
+	tilemap_set_scrolldy(system32_layer_tilemap[layer], sys32_videoram[0x1ff32/2 + 2 * layer], -sys32_videoram[0x1ff32/2 + 2 * layer]);
 	tilemap_draw(bitmap,&clip,system32_layer_tilemap[layer],trans,0);
 
 	/* enable this code below to display zoom information */
 #if 1
 	if (dstxstep != 0x200 || dstystep != 0x200)
 		usrintf_showmessage("Zoom=%03X,%03X  Cent=%03X,%03X", dstxstep, dstystep,
-			sys32_videoram[0x1ff30/2 + 2 * layer]&0x1ff,
-			sys32_videoram[0x1ff32/2 + 2 * layer]&0x1ff);
+			sys32_videoram[0x1ff30/2 + 2 * layer],
+			sys32_videoram[0x1ff32/2 + 2 * layer]);
 #endif
 }
 
