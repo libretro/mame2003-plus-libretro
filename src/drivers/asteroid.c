99b6b06adf19e0ff095c3fd972d4be2f9fe96156
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2024-01-26 09:41:29 -0500

    Update asteroid.c

--- src/drivers/asteroid.c
+++ src/drivers/asteroid.c
@@ -156,8 +156,9 @@
 static WRITE_HANDLER( cocktail_inv_w )
 {
 	/* player selection is bit 0x04 */
-	avg_set_flip_x( (readinputport(optional_io_port) && (data & 0x04))?1:0 );
-	avg_set_flip_y( (readinputport(optional_io_port) && (data & 0x04))?1:0 );
+	optional_io_active = (readinputport(optional_io_port) && (data & 0x04))?1:0;
+	avg_set_flip_x( optional_io_active );
+	avg_set_flip_y( optional_io_active );
 }
 
 
