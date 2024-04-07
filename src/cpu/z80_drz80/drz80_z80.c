8adee9fb6477a5d57ce9b432fe17a2ec5fa86472
Grantonius maximus <136645825+grant2258@users.noreply.github.com>
2024-04-07 23:44:02 +0100

    update

--- src/cpu/z80_drz80/drz80_z80.c
+++ src/cpu/z80_drz80/drz80_z80.c
@@ -279,7 +279,7 @@ void drz80_set_reg (int regnum, unsigned val)
 void drz80_set_irq_line(int irqline, int state)
 {
 	printf("drz80_set_irq_line(irqline:%d, state:%d\n",rqline, state);
-	log_cb(RETRO_LOG_INFO, "drz80_set_irq_line(irqline:%d, state:%d\n",rqline, state);
+	log_cb(RETRO_LOG_INFO, "drz80_set_irq_line(irqline:%d, state:%d\n",irqline, state);
 	if(irqline == IRQ_LINE_NMI){
 		DRZ80.nmi_state=state;
 		if (state!=CLEAR_LINE)
