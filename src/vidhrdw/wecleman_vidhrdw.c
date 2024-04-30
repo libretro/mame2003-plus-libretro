80d41e024546d07c14d4b89f61df2d1ef6fde580
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2024-04-30 13:49:22 -0400

    02996 hotchase: [debug] Access Violation [Phil Bennett]

--- src/vidhrdw/wecleman_vidhrdw.c
+++ src/vidhrdw/wecleman_vidhrdw.c
@@ -55,6 +55,7 @@ static int gameid, spr_offsx, spr_offsy, spr_count;
 static UINT16 *rgb_half;
 
 static int cloud_blend, cloud_ds, cloud_visible;
+static pen_t black_pen;
 
 
 /***************************************************************************
@@ -324,7 +325,10 @@ static void do_blit_zoom16(struct mame_bitmap *bitmap, const struct rectangle *c
 					if (pix != 0xa)
 						dst_ptr[sx] = pal_base[pix];
 					else
-						dst_ptr[sx] |= 0x800;
+					{
+						if (dst_ptr[sx] != black_pen)
+							dst_ptr[sx] |= 0x800;
+					}
 				}
 				src_fpx += src_fdx;
 			}
@@ -901,6 +905,7 @@ VIDEO_START( wecleman )
 	cloud_blend = BLEND_MAX;
 	cloud_ds = 0;
 	cloud_visible = 0;
+	black_pen = get_black_pen();
 
 	rgb_half     =          (UINT16*)(buffer + 0x00000);
 	t32x32pm     =             (int*)(buffer + 0x10020);
@@ -1003,6 +1008,7 @@ VIDEO_START( hotchase )
 	wecleman_gfx_bank = bank;
 	spr_offsx = -0xc0;
 	spr_offsy = 0;
+	black_pen = get_black_pen();
 
 	spr_ptr_list = (struct sprite **)buffer;
 
@@ -1066,7 +1072,7 @@ VIDEO_UPDATE ( wecleman )
 
 	get_sprite_info();
 
-	fillbitmap(bitmap, get_black_pen(), cliprect);
+	fillbitmap(bitmap, black_pen, cliprect);
 
 	/* Draw the road (lines which have priority 0x02) */
 	if (video_on) wecleman_draw_road(bitmap, cliprect, 0x02);
@@ -1151,7 +1157,7 @@ VIDEO_UPDATE( hotchase )
 
 	get_sprite_info();
 
-	fillbitmap(bitmap, get_black_pen(), cliprect);
+	fillbitmap(bitmap, black_pen, cliprect);
 
 	/* Draw the background */
 	if (video_on) K051316_zoom_draw_0(bitmap,cliprect, 0, 0);
