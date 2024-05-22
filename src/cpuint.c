067a85900862166b8cfcc58a37fd374f77fe61b9
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2024-05-22 05:33:57 -0400

    Update cpuint.c

--- src/cpuint.c
+++ src/cpuint.c
@@ -448,7 +448,7 @@ INTERRUPT_GEN( irq7_line_assert )	{ irqn_line_assert(7); }
  *
  *************************************/
 
-static void cpu_clearintcallback(int cpunum)
+void cpu_clearintcallback(int cpunum)
 {
 	int irqcount = cputype_get_interface(Machine->drv->cpu[cpunum].cpu_type)->num_irqs;
 	int irqline;
