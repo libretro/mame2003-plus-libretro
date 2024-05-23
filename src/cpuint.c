38a8cc678ca7919d106691e92b12a44cd2002355
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2024-05-23 03:16:03 -0400

    Update cpuint.c

--- src/cpuint.c
+++ src/cpuint.c
@@ -312,7 +312,7 @@ void cpu_set_irq_line_and_vector(int cpunum, int irqline, int state, int vector)
 	INT32 irq_event = (state & 0xff) | ((irqline & 0xff) << 8) | (vector << 16);
 	int event_index;
 
-	//if (cpu_pause_state) return;
+	if (cpu_pause_state && state == HOLD_LINE) return;
 
 	event_index = irq_event_index[cpunum]++;
 
