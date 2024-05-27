0e12ade1bcd1a7f744f61f07179a8c977e1188ec
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2024-05-26 21:14:04 -0400

    Update cpuexec.c

--- src/cpuexec.c
+++ src/cpuexec.c
@@ -319,7 +319,7 @@ static void cpu_pre_run(void)
 	/* reset the globals */
 	cpu_vblankreset();
 	current_frame = 0;
-	cpu_pause(cpu_pause_state);
+	cpu_pause_state = false;
 	state_save_dump_registry();
 }
 
