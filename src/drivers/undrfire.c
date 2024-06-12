c6bdbbcb735d4e6ad249806cdf1d68f85a980cb2
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2024-06-12 10:23:18 -0400

    Update undrfire.c

--- src/drivers/undrfire.c
+++ src/drivers/undrfire.c
@@ -623,14 +623,16 @@ static MEMORY_WRITE32_START( cbombers_writemem )
 MEMORY_END
 
 static MEMORY_READ16_START( cbombers_sub_readmem )
-    { 0x000000, 0x03ffff, MRA16_ROM },
+	{ 0x000000, 0x03ffff, MRA16_ROM },
 	{ 0x400000, 0x40ffff, MRA16_RAM	}, /* local ram */
+	{ 0x600000, 0x60ffff, MRA16_RAM	},
 	{ 0x800000, 0x80ffff, shared_ram_r },
 MEMORY_END
 
 static MEMORY_WRITE16_START( cbombers_sub_writemem )
-    { 0x000000, 0x03ffff, MWA16_ROM },
-	{ 0x400000, 0x40ffff, MWA16_RAM	},/* local ram */
+	{ 0x000000, 0x03ffff, MWA16_ROM },
+	{ 0x400000, 0x40ffff, MWA16_RAM	}, /* local ram */
+	{ 0x600000, 0x60ffff, tc0480scp_word_w	}, /* Only written upon errors - cbombersp data on screen error */
 	{ 0x800000, 0x80ffff, shared_ram_w },
 MEMORY_END
 
