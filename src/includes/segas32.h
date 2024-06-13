3db2677bb090499b7af6b05732d8117cf89d5a8d
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2024-06-13 19:36:58 -0400

    Create segas32.h

---
+++ src/includes/segas32.h
@@ -0,0 +1,17 @@
+/***************************************************************************
+
+    Sega System 32/Multi 32 hardware
+
+***************************************************************************/
+
+
+/*----------- defined in video/segas32.c -----------*/
+
+extern UINT16 *system32_paletteram16[2];
+
+READ16_HANDLER( system32_paletteram_r );
+WRITE16_HANDLER( system32_paletteram_w );
+READ32_HANDLER( multi32_paletteram_0_r );
+WRITE32_HANDLER( multi32_paletteram_0_w );
+READ32_HANDLER( multi32_paletteram_1_r );
+WRITE32_HANDLER( multi32_paletteram_1_w );
