06a611662835b5685d8f9b34ba28bdfdc8ce6aab
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2024-06-14 04:06:29 -0400

    Update segas32.h

--- src/includes/segas32.h
+++ src/includes/segas32.h
@@ -16,7 +16,10 @@ WRITE32_HANDLER( multi32_paletteram_0_w );
 READ32_HANDLER( multi32_paletteram_1_r );
 WRITE32_HANDLER( multi32_paletteram_1_w );
 
-WRITE16_HANDLER( system32_mixer_w );
 READ16_HANDLER( system32_mixer_r );
+WRITE16_HANDLER( system32_mixer_w );
+
+READ32_HANDLER( multi32_mixer_0_r );
+READ32_HANDLER( multi32_mixer_1_r );
 WRITE32_HANDLER( multi32_mixer_0_w );
 WRITE32_HANDLER( multi32_mixer_1_w );
