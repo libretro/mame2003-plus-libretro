405620331ac4a430429b0cf4aa543c5218e0667f
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2024-06-13 23:53:42 -0400

    Update segas32.h

--- src/includes/segas32.h
+++ src/includes/segas32.h
@@ -8,6 +8,7 @@
 /*----------- defined in video/segas32.c -----------*/
 
 extern UINT16 *system32_paletteram[2];
+extern UINT16 *system32_mixerregs[2];
 
 READ16_HANDLER( system32_paletteram_r );
 WRITE16_HANDLER( system32_paletteram_w );
