457fb15604860f9ad1b2a025d4b19d7f02cd0846
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2024-06-14 02:19:38 -0400

    Update multi32.c

--- src/drivers/multi32.c
+++ src/drivers/multi32.c
@@ -39,7 +39,6 @@ extern data16_t sys32_displayenable;
 /* Video Hardware */
 extern int system32_temp_kludge;
 extern data16_t *sys32_spriteram16;
-extern data16_t *sys32_txtilemap_ram;
 static data16_t *paletteram16_b;
 
 extern int system32_mixerShift;
