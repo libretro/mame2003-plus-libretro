d3acc89d9bbd285c417977592e595ee3150a4460
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2024-06-16 16:41:01 -0400

    Update segas32.c

--- src/vidhrdw/segas32.c
+++ src/vidhrdw/segas32.c
@@ -1186,9 +1186,12 @@ static void draw_bg_layer(struct mame_bitmap *bitmap, const struct rectangle *cl
 
 		/* get the 5-5-5 color and convert to 8-8-8 */
 		color = system32_paletteram[(basecol + (color & 0x0f) + ((color >> shift) & 0x1ff0)) & 0x3fff];
-		r = rlookup[color & 0x1f];
+		/*r = rlookup[color & 0x1f];
 		g = glookup[(color >> 5) & 0x1f];
-		b = blookup[(color >> 10) & 0x1f];
+		b = blookup[(color >> 10) & 0x1f];*/
+		int r = color & 0x1f;
+		int g = (color >> 5) & 0x1f;
+		int b = (color >> 10) & 0x1f;
 		color = (r << 16) | (g << 8) | b;
 
 		/* loop over columns */
@@ -1321,9 +1324,9 @@ static void draw_sprite_layers(struct mame_bitmap *bitmap, const struct rectangl
 						else
 						{
 							UINT16 color = system32_paletteram[(basecol[layer] + (pix & 0x07ff)) & 0x3fff];
-							int r = rlookup[color & 0x1f];
-							int g = glookup[(color >> 5) & 0x1f];
-							int b = blookup[(color >> 10) & 0x1f];
+							int r = color & 0x1f;
+							int g = (color >> 5) & 0x1f;
+							int b = (color >> 10) & 0x1f;
 							dst[x] = (r << 16) | (g << 8) | b;
 						}
 					}
