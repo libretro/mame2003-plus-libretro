3bb9ce511d090bb863cd2d48568b572cbcc2b3d3
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2024-06-17 17:50:38 -0400

    Update segas32.c

--- src/vidhrdw/segas32.c
+++ src/vidhrdw/segas32.c
@@ -371,7 +371,7 @@ static INLINE UINT16 xBBBBBGGGGGRRRRR_to_xBGRBBBBGGGGRRRR(UINT16 value)
 	int g = (value >> 5) & 0x1f;
 	int b = (value >> 10) & 0x1f;
 	value = (value & 0x8000) | ((b & 0x01) << 14) | ((g & 0x01) << 13) | ((r & 0x01) << 12);
-	value |= ((b & 0x1e) << 7) | ((g & 0x1e) << 3) | ((r & 0x1e) >> 1);
+	value |= ((b & 0x1e) << 7) | ((g & 0x1e) << 3) | ((b & 0x1e) >> 1);
 	return value;
 }
 
