e93b4e35af2dec1401779ab7a451fcdd9fde08c8
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2024-06-16 21:12:43 -0400

    Update segas32.c

--- src/vidhrdw/segas32.c
+++ src/vidhrdw/segas32.c
@@ -1167,9 +1167,9 @@ static void draw_bg_layer(struct mame_bitmap *bitmap, const struct rectangle *cl
 
 	/* check the color select bit, and get pointers to the lookup tables */
 	colselect = (mixer_control[MONITOR][0x3e/2] >> 14) & 1;
-	rlookup = &clamp_and_expand[32 + ((INT8)(mixer_control[MONITOR][0x40/2 + colselect * 3] << 2) >> 2)];
-	glookup = &clamp_and_expand[32 + ((INT8)(mixer_control[MONITOR][0x42/2 + colselect * 3] << 2) >> 2)];
-	blookup = &clamp_and_expand[32 + ((INT8)(mixer_control[MONITOR][0x44/2 + colselect * 3] << 2) >> 2)];
+	rlookup = &clamp_and_expand[((INT8)(mixer_control[MONITOR][0x40/2] << 2) >> 2)];
+	glookup = &clamp_and_expand[((INT8)(mixer_control[MONITOR][0x42/2] << 2) >> 2)];
+	blookup = &clamp_and_expand[((INT8)(mixer_control[MONITOR][0x44/2] << 2) >> 2)];
 
 	/* loop over rows with non-zero checksums */
 	for (y = cliprect->min_y; y <= cliprect->max_y; y++)
