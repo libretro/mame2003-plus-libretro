abaf52d4ece40d071c7cd2bf52d58c1370c14c88
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2024-07-18 05:50:53 -0400

    Small tweak

--- src/vidhrdw/segas32.c
+++ src/vidhrdw/segas32.c
@@ -1782,7 +1782,7 @@ static int draw_one_sprite(UINT16 *data, int xoffs, int yoffs, const struct rect
 	};
 
 	struct mame_bitmap *bitmap = layer_data[(!is_multi32 || !(data[3] & 0x0800)) ? MIXER_LAYER_SPRITES_2 : MIXER_LAYER_MULTISPR_2].bitmap;
-	UINT8 numbanks = (memory_region_length(REGION_GFX2) >> 20)/4;
+	UINT8 numbanks = (memory_region_length(REGION_GFX2) >> 20)/sizeof(UINT32); /* memory_region_length in bytes */
 	const UINT32 *spritebase = (const UINT32 *)memory_region(REGION_GFX2);
 
 	int indirect = data[0] & 0x2000;
