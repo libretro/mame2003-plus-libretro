85029a49c22142b38fd99b30522e0b4a7fb8dd9b
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2024-08-21 21:45:17 -0400

    Update usrintrf.c

--- src/usrintrf.c
+++ src/usrintrf.c
@@ -3437,7 +3437,7 @@ int handle_user_interface(struct mame_bitmap *bitmap)
 
 	if(toggle_showgfx) showcharset(bitmap);
 
-	if (!setup_active() && !toggle_showgfx && cpu_pause_state)
+	if (!setup_active() && !toggle_showgfx && pause_action)
 		cpu_pause(false);
 
 	return 0;
