090940be8bd7a81c32fe2a0f7e4d81c15c1db3a6
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2024-10-07 11:41:08 -0400

    Update segas32.c

--- src/vidhrdw/segas32.c
+++ src/vidhrdw/segas32.c
@@ -1638,7 +1638,7 @@ static UINT8 update_tilemaps(const struct rectangle *cliprect)
 	if (enableb)
 		update_bitmap(&layer_data[MIXER_LAYER_BITMAP], cliprect);
 	update_background(&layer_data[MIXER_LAYER_BACKGROUND], cliprect);
-usrintf_showmessage("t:%i  e0:%i  e1:%i  e2:%i  e3:%i  b:%i  com:%i", enablet, enable0, enable1, enable2, enable3, enableb, (enablet << 0) | (enable0 << 1) | (enable1 << 2) | (enable2 << 3) | (enable3 << 4) | (enableb << 5) );
+
 	return (enablet << 0) | (enable0 << 1) | (enable1 << 2) | (enable2 << 3) | (enable3 << 4) | (enableb << 5);
 }
 
@@ -2718,7 +2718,7 @@ VIDEO_UPDATE( multi32 )
 	int monitor_setting = readinputport(0xf);
 	int monitor_display_start = 0;
 	int monitor_display_width = 2;
-	int monitor_vertical_offset = 2;
+	int monitor_vertical_offset = 1;
 
 /*
    MAME2003-PLUS uses a single screen to draw to where as current mame
@@ -2762,7 +2762,7 @@ VIDEO_UPDATE( multi32 )
 
 	/* update the tilemaps */
 	profiler_mark(PROFILER_USER1);
-	enablemask = update_tilemaps(&clipright);
+	enablemask = update_tilemaps(&clipleft);
 	profiler_mark(PROFILER_END);
 
 	/* debugging */
