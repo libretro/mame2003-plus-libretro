5304594287a4cf388ae45337c64a8b027958bb49
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2024-10-09 08:44:31 -0400

    Multi32 fix aspect ratio (#1865)

--- src/vidhrdw/segas32.c
+++ src/vidhrdw/segas32.c
@@ -2712,31 +2712,36 @@ for (showclip = 0; showclip < 4; showclip++)
 
 VIDEO_UPDATE( multi32 )
 {
+	extern struct osd_create_params video_config;
 	struct rectangle clipleft, clipright;
 	UINT8 enablemask;
   
 	int monitor_setting = readinputport(0xf);
 	int monitor_display_start = 0;
 	int monitor_display_width = 2;
-	int monitor_vertical_offset = 2;
 
 /*
    MAME2003-PLUS uses a single screen to draw to where as current mame
-   uses dedicated left and right screens. The machine driver doubles the
-   y max to allow for this so we must manually set our y clip max to the
-   actual maximum for drawing purposes.
+   uses dedicated left and right screens. We force an aspect ratio change
+   to maintain the correct 4:3 ratio across single or dual monitors.
 */
 	if (monitor_setting != 3)
 	{
 		monitor_display_start = monitor_setting - 1;
 		monitor_display_width = monitor_setting;
-		monitor_vertical_offset = 1;
+		video_config.aspect_x = 4;
+		video_config.aspect_y = 3;
+	}
+	else
+	{
+		video_config.aspect_x = 8;
+		video_config.aspect_y = 3;
 	}
 
 	/* update the visible area */
 	if (system32_videoram[0x1ff00/2] & 0x8000)
 	{
-		set_visible_area(52*monitor_display_start*8, 52*8*monitor_display_width-1, 0, 28*8*monitor_vertical_offset-1);
+		set_visible_area(52*monitor_display_start*8, 52*8*monitor_display_width-1, 0, 28*8-1);
 		clipleft.min_x = 0;
 		clipleft.max_x = 52*8-1;
 		clipright.min_x = 52*8;
@@ -2744,14 +2749,14 @@ VIDEO_UPDATE( multi32 )
 	}
 	else
 	{
-		set_visible_area(40*monitor_display_start*8, 40*8*monitor_display_width-1, 0, 28*8*monitor_vertical_offset-1);
+		set_visible_area(40*monitor_display_start*8, 40*8*monitor_display_width-1, 0, 28*8-1);
 		clipleft.min_x = 0;
 		clipleft.max_x = 40*8-1;
 		clipright.min_x = 40*8;
 		clipright.max_x = 40*2*8-1;
 	}
 	clipleft.min_y = clipright.min_y = cliprect->min_y;
-	clipleft.max_y = clipright.max_y = 28*8-1;
+	clipleft.max_y = clipright.max_y = cliprect->max_y;
 
 	/* if the display is off, punt */
 	if (!system32_displayenable[0] && !system32_displayenable[1])
