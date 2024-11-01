e6802571c64f44d8bb5d625dc7f018a0767f46a4
arcadez2003 <51386955+arcadez2003@users.noreply.github.com>
2024-11-01 23:19:31 +0000

    jparkmb

--- src/driver.c
+++ src/driver.c
@@ -3255,6 +3255,7 @@ BOMULEUL CHAJARA SEGA ST-V  1997/04/11
   DRIVER( barek2ch ) /* (c) 1994 bootleg / Sega */
   DRIVER( sonic2mb ) /* (c) 1993 bootleg / Sega */
   DRIVER( sonic3mb ) /* (c) 1993 bootleg / Sega */
+  DRIVER( jparkmb )  /* (c) 1993 bootleg / Sega */
 
 	/* Sega MegaTech, the number shown ia on the label of the instruction rom */
 	DRIVER( mt_beast )	/* 01 */
