627eb92a58270e14eb8f67c8878ba0aa587ea9e2
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2024-11-13 14:43:41 -0500

    Clean2

--- src/sound/multipcm.h
+++ src/sound/multipcm.h
@@ -23,8 +23,6 @@ WRITE_HANDLER( MultiPCM_reg_0_w );
 READ_HANDLER( MultiPCM_reg_0_r);
 WRITE_HANDLER( MultiPCM_reg_1_w );
 READ_HANDLER( MultiPCM_reg_1_r);
-WRITE_HANDLER( MultiPCM_bank_0_w );
-WRITE_HANDLER( MultiPCM_bank_1_w );
 
 void multipcm_set_bank(int which, UINT32 leftoffs, UINT32 rightoffs);
 
