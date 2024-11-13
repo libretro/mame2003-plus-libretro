9c4cea3208335841fc14487c7013d8d791a00276
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2024-11-13 01:33:15 -0500

    Update multipcm.h

--- src/sound/multipcm.h
+++ src/sound/multipcm.h
@@ -3,11 +3,6 @@
 
 #define MAX_MULTIPCM	(2)	/* max # of multipcm chips*/
 
-/* banking types (done via an external PAL, so can vary per game)*/
-#define MULTIPCM_MODE_MODEL1	(0)	/* Model 1 banking method*/
-#define MULTIPCM_MODE_MULTI32	(1)	/* Multi32 banking method*/
-#define MULTIPCM_MODE_STADCROSS (2)	/* Stadium Cross banking method*/
-
 #ifndef VOL_YM3012
 #define YM3012_VOL(LVol,LPan,RVol,RPan) (MIXER(LVol,LPan)|(MIXER(RVol,RPan) << 16))
 #endif
