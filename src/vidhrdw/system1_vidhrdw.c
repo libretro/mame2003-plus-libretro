f703aa6d5c1c6fbd481009738a81e476ccb79381
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2025-02-07 04:54:24 -0500

    Update system1_vidhrdw.c

--- src/vidhrdw/system1_vidhrdw.c
+++ src/vidhrdw/system1_vidhrdw.c
@@ -692,7 +692,7 @@ VIDEO_UPDATE( shtngmst )
 
 
 	draw_crosshair(1, bitmap, readinputport(6) * (Machine->drv->screen_width-1) / 0xff,
-	                          (Machine->drv->screen_height-1) - (readinputport(7) * (Machine->drv->screen_height-1) / 0xff),
+	                          (Machine->drv->screen_height-1) - (readinputport(7) * (Machine->drv->screen_height-1) / 0xff + 8),
 	                          &Machine->visible_area);
 
 #ifdef MAME_DEBUG
