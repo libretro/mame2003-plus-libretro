2275a3787985a12970eb91eefeaecbd8e6034c05
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2025-02-09 11:09:49 -0500

    Update system1_vidhrdw.c

--- src/vidhrdw/system1_vidhrdw.c
+++ src/vidhrdw/system1_vidhrdw.c
@@ -274,7 +274,7 @@ static void draw_sprite(struct mame_bitmap *bitmap,int spr_number)
 		x = sprite_base[SPR_X_LO] + ((sprite_base[SPR_X_HI] & 0x01) << 8) + 1;
 		if (Machine->gamedrv == &driver_wbml || Machine->gamedrv->clone_of == &driver_wbml ||
 			Machine->gamedrv == &driver_ufosensi || Machine->gamedrv->clone_of == &driver_ufosensi ||
-			Machine->gamedrv == &driver_shtngmst )
+			(!strcmp(Machine->gamedrv->name, "shtngmst")) )
 		{
 			x += 7*2;
 		}
