30e848d95c48265626642b3c27b2d4fbfec0cfba
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2025-05-08 13:33:39 -0400

    Update dbz2.c

--- src/drivers/dbz2.c
+++ src/drivers/dbz2.c
@@ -492,8 +492,6 @@ MACHINE_DRIVER_END
 
 /**********************************************************************************/
 
-#define ROM_LOAD64_WORD(name,offset,length,crc)		ROMX_LOAD(name, offset, length, crc, ROM_GROUPWORD | ROM_SKIP(6))
-
 ROM_START( dbz )
 	/* main program */
 	ROM_REGION( 0x400000, REGION_CPU1, 0)
