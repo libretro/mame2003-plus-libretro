dbc1d874b3bf3a9af0d45b272a539f3b87faa1db
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2025-09-19 06:14:57 -0400

    Update segas32.c

--- src/vidhrdw/segas32.c
+++ src/vidhrdw/segas32.c
@@ -1677,11 +1677,11 @@ static UINT8 update_tilemaps(const struct rectangle *cliprect)
 		update_tilemap_zoom(&layer_data[MIXER_LAYER_NBG1], cliprect, 1);
 	*/if (enable2)
 		update_tilemap_rowscroll(&layer_data[MIXER_LAYER_NBG2], cliprect, 2);
-	if (enable3)
+	/*if (enable3)
 		update_tilemap_rowscroll(&layer_data[MIXER_LAYER_NBG3], cliprect, 3);
-	/*if (enablet)
+	*/if (enablet)
 		update_tilemap_text(&layer_data[MIXER_LAYER_TEXT], cliprect);
-	if (enableb)
+	/*if (enableb)
 		update_bitmap(&layer_data[MIXER_LAYER_BITMAP], cliprect);
 	if (!titlef_kludge)
 		update_background(&layer_data[MIXER_LAYER_BACKGROUND], cliprect);
