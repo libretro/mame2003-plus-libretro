515c252f4bb93bc7cb4d12d9a4300539c4e52395
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2026-09-01 11:56:03 -0400

    Update segas32.c

--- src/vidhrdw/segas32.c
+++ src/vidhrdw/segas32.c
@@ -2722,7 +2722,7 @@ VIDEO_UPDATE( multi32 )
 		fillbitmap(bitmap, get_black_pen(), cliprect);
 		return;
 	}
-titlef_kludge = false;
+
 	/* update the tilemaps */
 	enablemask = update_tilemaps(&clipleft);
 
